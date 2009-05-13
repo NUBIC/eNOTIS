861517f9f7b728068c0800ec761bf0c1ba14cc37
Brian Chamberlain <b-chamberlain@northwestern.edu>
2009-05-13 12:25:27 -0500

    tests for controllers pass but app is still broke

--- patient_tracker/spec/controllers/authentication_controller_spec.rb
+++ patient_tracker/spec/controllers/authentication_controller_spec.rb
@@ -7,9 +7,22 @@ describe AuthenticationController do
     controller.should be_an_instance_of(AuthenticationController)
   end
 
-  it "accepts credentials and authenticates them" do
-    account = User.new
-    User.should_receive(:validate_user).with("abc123","blah").and_return(account)
-    post 'login', :netid => 'abc123',:password => 'blah'
+  describe "the login process" do
+    before(:each) do
+      @account = User.new
+      @account.netid = 'abc123'
+      User.stub!(:validate_user).and_return(@account)
+    end
+      
+    it "accepts credentials and authenticates them" do
+      User.should_receive(:validate_user).with("abc123","blah").and_return(@account)
+      post 'login', :netid => 'abc123',:password => 'blah'
+    end
+
+    it "sets the current user in the session after a successful login" do
+      post 'login', :netid => 'abc123'
+      response.should be_redirect # to default page
+      session[:current_user].should == @account.id
+    end
   end
 end
