01cc75171d0c27c62a94f9c7c39c9b88258f56fe
Brian Chamberlain <b-chamberlain@northwestern.edu>
2009-05-14 10:52:18 -0500

    removed some files that shouldn't be tracked, updated .gitignore file, removed fixtures folder from specs (we don't use fixtures cause they're bad)

--- patient_tracker/db/schema.rb
+++ patient_tracker/db/schema.rb
@@ -9,47 +9,67 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20090512205121) do
+ActiveRecord::Schema.define(:version => 20090513203524) do
 
-  create_table "patient_on_protocol_status", :force => true do |t|
-    t.integer "patient_on_protocol_id"
-    t.string  "status"
-    t.date    "start_date"
+  create_table "involvements", :force => true do |t|
+    t.integer  "patient_id"
+    t.integer  "protocol_id"
+    t.string   "disease_site"
+    t.text     "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "patient_events", :force => true do |t|
+    t.integer  "patient_id"
+    t.string   "status"
+    t.date     "status_date"
+    t.integer  "protocol_id"
+    t.text     "notes"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "patients", :force => true do |t|
-    t.string "mrd_pt_id"
-    t.string "type"
-    t.string "mrn"
-    t.string "mrn_type"
-    t.string "last_name"
-    t.string "first_name"
-    t.date   "birth_date"
-    t.date   "death_date"
-    t.string "address_line1"
-    t.string "address_line2"
-    t.string "city"
-    t.string "state"
-    t.string "zip"
-    t.string "phone"
-    t.string "work_phone"
-    t.string "work_phone_extension"
-  end
-
-  create_table "patients_on_protocols", :force => true do |t|
-    t.integer  "patient_id"
-    t.integer  "protocol_id"
-    t.integer  "patient_on_protocol_status_id"
+    t.string   "mrn"
+    t.string   "mrn_type"
+    t.string   "source"
+    t.date     "last_reconciled"
+    t.string   "reconcile_status"
+    t.string   "last_name"
+    t.string   "first_name"
+    t.boolean  "lost_to_follow_up"
+    t.text     "lost_to_follow_up_reason"
+    t.date     "birth_date"
+    t.date     "death_date"
+    t.string   "address_line1"
+    t.string   "address_line2"
+    t.string   "city"
+    t.string   "state"
+    t.string   "zip"
+    t.string   "phone"
+    t.string   "work_phone"
+    t.string   "work_phone_extension"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
   create_table "protocols", :force => true do |t|
-    t.string "irb_number"
+    t.string   "irb_number"
+    t.string   "title"
+    t.string   "description"
+    t.string   "approval_status"
+    t.date     "reconciliation_date"
+    t.string   "reconciliation_status"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "users", :force => true do |t|
     t.string   "netid"
+    t.string   "email"
+    t.string   "first_name"
+    t.string   "last_name"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
