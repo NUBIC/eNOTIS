dc2c0778f94a6b23841a52bab434d8b729c5da7a
kumbafu <mutero_d@yahoo.com>
2009-05-14 10:27:51 -0500

    added involvements call to protocol/show to show patients

--- patient_tracker/app/controllers/protocol_controller.rb
+++ patient_tracker/app/controllers/protocol_controller.rb
@@ -1,13 +1,12 @@
 class ProtocolController < ApplicationController
-
-	def index
-	  @protocols = Protocol.find_by_coordinator(params[:net_id])
-	end
-	
-	def show
-	  @protocol = Protocol.find_by_study_id(params[:study_id])
-	end
-
-
-	
+ 
+  def index
+   @protocols = Protocol.find_by_coordinator(params[:netid])
+  end
+  
+  def show
+   @protocol = Protocol.find_by_study_id(params[:study_id])
+   @patients = Involvement.find_by_protocol_id(@protocol)
+  end
+  
 end
