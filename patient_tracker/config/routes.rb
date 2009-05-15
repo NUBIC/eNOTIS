fbfdd8524d702634b6223a9e0445837622a03ea7
kumbafu <mutero_d@yahoo.com>
2009-05-15 02:12:16 -0500

    pushing code for patient registrations. Pretty dirty, still has the erb files, just need to upoad for safety

--- patient_tracker/config/routes.rb
+++ patient_tracker/config/routes.rb
@@ -1,6 +1,6 @@
 ActionController::Routing::Routes.draw do |map|
   map.resources :authentication, :collection => {:login => :post, :logout => :any}
-  map.resources :registrations
+  map.resources :registrations,:collection => {:search => :post,:add_patient => :post }
   map.resources :protocols, :collection => {:search => :get}
   map.resources :patients, :collection => {:search => :get}
   map.default 'registration', {:controller => "registrations", :action => "index"}
