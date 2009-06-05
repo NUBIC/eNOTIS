232098589dc68dbc190c08f9dab9de56c7f2e342
Brian Chamberlain <b-chamberlain@northwestern.edu>
2009-06-05 10:50:05 -0500

    merged in eirb changes from spec work

--- patient_tracker/lib/eirb_services.rb
+++ patient_tracker/lib/eirb_services.rb
@@ -1,4 +1,4 @@
-
+require 'eirb_adapter'
 
 module EirbServices
 
@@ -6,20 +6,25 @@ module EirbServices
                    :numRows => -1,
                    :expandMultiValueCells => false}.freeze
   
-  class EirbService
+  class EirbService 
 
+    # initialzes the service class 
     def initialize
-      config = ServiceConfig.new(RAILS_ENV)
-
-      @adapter = EirbAdapter(config)
+      yml = File.open(File.join(RAILS_ROOT,"config/eirb_services.yml"))
+      config = ServiceConfig.new(RAILS_ENV, YAML.parse(yml))
+      @adapter = EirbAdapter.new(config)
     end
 
-     # Performs a query to get the study status using the study ID 
-     def find_status_by_id(study_id)
-       # merging in the defaults, overwritting ones we need to
-       search_settings = SEARCH_DEFAULTS.merge({:savedSearchName => "idStatus",
-                                               :parameters => {:id => study_id}})
-       @adapter.perform_search(search_settings)  
-     end
+    # Performs a query to get the study status using the study ID 
+    def find_status_by_id(study_id)
+      # merging in the defaults, overwritting ones we need to
+      search_settings = SEARCH_DEFAULTS.merge({:savedSearchName => "idStatus",
+                                              :parameters => {:id => study_id}})
+      @adapter.perform_search(search_settings)  
+    end
+
+    def find_by_study_id(study_id)
+
+    end
   end
 end
--- patient_tracker/spec/lib/eirb_services_spec.rb
+++ patient_tracker/spec/lib/eirb_services_spec.rb
@@ -1,31 +1,18 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 require File.expand_path(File.dirname(__FILE__) + '/../soap_mock_helper')
+require 'eirb_services'
 
 describe EirbServices do
   
   describe "search results" do
-    it "returns an empty array for a search with no results" do
-     empty_search = SoapMockHelper::Search.new.results([])
-     @driver.should_receive(:performSearch).and_return(empty_search)
-     @tester.find_status_by_id("STU123123123").should == []
+    before(:each) do
+      @service = EirbServices::EirbService.new
     end
-    
-    it "returns the results as a hash for one row results" do
-      payload = [{"ID" => "STU0912301239"}, {"Project State.ID" => "APPROVED"}, {"Name" => "The research study"}, {"Research Type.ID" => "INTERVENTIONAL"}]
-      results = SoapMockHelper::Search.new.results(payload)
-      @driver.should_receive(:performSearch).and_return(results)
-      @tester.find_status_by_id("STU123123123").should == [{"ID" => "STU0912301239", "Project State.ID" => "APPROVED", "Name" => "The research study", "Research Type.ID" => "INTERVENTIONAL"}]
+
+    it "can find the status of a study by id" do
+      @service.find_status_by_id("STU123123123").should == []
+      @service.find_status_by_id("STU123123123").should == [{"ID" => "STU00000706", "Project State.ID" => "Approved"} ]
     end
       
-    it "returns the results as an array of hashes for multi-row results" do
-      payload = [[{"ID" => "STU0912301239"}, {"Project State.ID" => "APPROVED"}, {"Name" => "The research study"}, {"Research Type.ID" => "INTERVENTIONAL"}],
-        [{"ID" => "STU01239"}, {"Project State.ID" => "Terminated"}, {"Name" => "The other research study"}, {"Research Type.ID" => "INTERVENTIONAL"}]]
-      results = SoapMockHelper::Search.new.results(payload)
-      @driver.should_receive(:performSearch).and_return(results)
-      # it doesnt matter that this particular method will never return 
-      # multiple values. We are just testing the end to end process.
-      @tester.find_status_by_id("STU123123123").should == [{"ID" => "STU0912301239", "Project State.ID" => "APPROVED", "Name" => "The research study", "Research Type.ID" => "INTERVENTIONAL"},
-        {"ID" => "STU01239", "Project State.ID" => "Terminated", "Name" => "The other research study", "Research Type.ID" => "INTERVENTIONAL"}]
-    end
   end
 end
