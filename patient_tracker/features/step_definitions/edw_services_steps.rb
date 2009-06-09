9951519ace680ef71b7304c7e0a1fd5d50f5cd12
Mark Yoon <yoon@northwestern.edu>
2009-06-09 16:38:33 -0500

    working on edw feature steps

---
+++ patient_tracker/features/step_definitions/edw_services_steps.rb
@@ -0,0 +1,23 @@
+Given /^there is a patient with MRN ([a-zA-Z0-9]+) named ([\w\s]+)$/ do
+  pending
+end
+
+When /^I find a patient by MRN ([a-zA-Z0-9]+)$/ do
+  pending
+end
+
+Then /^I should get a patient named ([\w\s]+)$/ do
+  pending
+end
+
+Given /^there is a patient named ([\w\s]+) with birthday ([0-9\/\-\. ]+) with address ([\w\s]+)$/ do
+  pending
+end
+
+When /^I find a patient by name ([\w\s]+) and birthday ([0-9\/\-\. ]+)$/ do
+  pending
+end
+
+Then /^I should get a patient with address ([\w\s]+)$/ do
+  pending
+end
