d8ec0ad8e72aa48aafd01633095c7f9db0fb536d
Mark Yoon <yoon@northwestern.edu>
2009-06-10 16:15:44 -0500

    working on edw services spec

--- patient_tracker/spec/lib/edw_services_spec.rb
+++ patient_tracker/spec/lib/edw_services_spec.rb
@@ -7,25 +7,41 @@ describe EdwServices do
     service_klass = Class.new do 
       include EdwServices
     end
+    
+    @adapter = mock(EirbAdapter) 
+    @adapter.stub!(:perform_search)
+    
     @service = service_klass.new 
     @service.stub!(:connect)
-    @adapter = mock(EirbAdapter) 
+    @service.stub!(:edw_adapter).and_return(@adapter)
+
   end
   
   describe "finding data about patients" do
     it "can find the details of a patient by mrn" do
       p = {:mrn => '9021090210'}
       @adapter.should_receive(:perform_search).with(p)
-      @service.stub!(:edw_adapter).and_return(@adapter)
       @service.find_by_mrn('9021090210')
+
     end
+    
     it "can find a list of patients by name or dob" do
-      pending
+      p = {:name => 'July Fourth', :dob => '7/4/50'}
+      @adapter.should_receive(:perform_search).with(p)
+      @service.find_by_name_and_dob("July Fourth", '7/4/50')
+
     end
   end 
+  
   it "always connects" do
-    pending
+    @service.should_receive(:connect)
+    @service.find_by_mrn("314")
+
+    @service.should_receive(:connect)
+    @service.find_by_name_and_dob("July Fourth", '7/4/50')
+  
   end
+  
   it "gives a meaningful error when it can't connect" do
     pending
   end
