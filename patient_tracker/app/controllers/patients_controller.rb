dfce959607589b9921bd3326a0e8817521800f5d
Mark Yoon <yoon@northwestern.edu>
2009-06-16 11:28:36 -0500

    starting specs on csv import

--- patient_tracker/app/controllers/patients_controller.rb
+++ patient_tracker/app/controllers/patients_controller.rb
@@ -22,4 +22,8 @@ class PatientsController < ApplicationController
   def search
     @patients = Patient.find_by_mrn(params[:mrn])
   end
+  
+  def create
+    render :layout => false, :text => ""
+  end
 end
--- patient_tracker/spec/controllers/patients_controller_spec.rb
+++ patient_tracker/spec/controllers/patients_controller_spec.rb
@@ -6,5 +6,24 @@ describe PatientsController do
   it "should use PatientsController" do
     controller.should be_an_instance_of(PatientsController)
   end
+  
+  describe "importing csv" do
+    # post file to create  
+    # check file
+    # spit it out to csv with errors
+    # redirect to x with success
+    # process file
+    # take me to a processing page
 
+    before(:each) do
+      good_csv = "mrn,first_name,last_name,dob\r\n9988101,,,\r\n9988102,,,\r\n,test10,last10,12/31/04\r\n,test11,last11,6/11/54\r\n,test12,last12,6/12/54\r\n,test13,last13,6/13/55\r\n,test14,last14,7/26/74\r\n,test15,last15,12/31/04\r\n,test16,last16,6/12/54\r\n"
+      File.new('good.csv', 'w').syswrite(good_csv)
+      @good_csv_file = File.open('good.csv')
+      controller.stub!(:user_must_be_logged_in)
+    end
+    it "should allow me to post the file to create" do
+      post :create, {:file => @good_csv_file}
+      response.should be_success
+    end
+  end
 end
