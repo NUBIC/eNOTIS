43b08f144bc142210a7dfc4f7f404aa7e1e11fab
Mark Yoon <yoon@northwestern.edu>
2009-06-16 17:10:02 -0500

    added more csv import specs and code to subjects controller

--- patient_tracker/app/controllers/subjects_controller.rb
+++ patient_tracker/app/controllers/subjects_controller.rb
@@ -1,11 +1,11 @@
 class SubjectsController < ApplicationController
+  require 'fastercsv'
   include AuthMod
   include FaceboxRender
   before_filter :user_must_be_logged_in
   layout "layouts/loggedin"
 
   def index
-    
     @protocol = Study.find_by_irb_number(params[:irb_number])
     @involvements = @protocol.involvements
   end
@@ -24,6 +24,29 @@ class SubjectsController < ApplicationController
   end
   
   def create
-    render :layout => false, :text => ""
+    if self.check_csv params[:file]
+      if params[:study]
+        redirect_to study_path(:id => params[:study])
+      else
+        render :layout => false, :text => ""
+      end
+    else
+      filename = "foo.csv"
+      headers["Content-Disposition"] = "attachment; filename='#{filename}'"
+      if request.env['HTTP_USER_AGENT'] =~ /msie/i
+        headers.merge({'Pragma' => 'public', "Content-type" => "text/plain", 'Cache-Control' => 'no-cache, must-revalidate, post-check=0, pre-check=0', 'Expires' => "0"})
+      else
+        headers["Content-Type"] ||= 'text/csv'        
+      end
+      send_data(params[:file].to_s, :type => 'text/csv; charset=utf-8; header=present')
+    end
+  end
+  
+  def self.check_csv(file)
+    arr_of_arrs = FasterCSV.read(file.path)
+    bool_arr = arr_of_arrs.map do |row|
+      !row.first.blank? or (!row[1].blank? and !row[2].blank? and !row[3].blank?)
+    end
+    bool_arr.uniq == [true]
   end
 end
--- patient_tracker/spec/controllers/subjects_controller_spec.rb
+++ patient_tracker/spec/controllers/subjects_controller_spec.rb
@@ -10,20 +10,41 @@ describe SubjectsController do
   describe "importing csv" do
     # post file to create  
     # check file
-    # spit it out to csv with errors
+    # spit it out to csv with errors in another column (check number and names of columns)
     # redirect to x with success
     # process file
     # take me to a processing page
 
     before(:each) do
-      good_csv = "mrn,first_name,last_name,dob\r\n9988101,,,\r\n9988102,,,\r\n,test10,last10,12/31/04\r\n,test11,last11,6/11/54\r\n,test12,last12,6/12/54\r\n,test13,last13,6/13/55\r\n,test14,last14,7/26/74\r\n,test15,last15,12/31/04\r\n,test16,last16,6/12/54\r\n"
-      File.new('good.csv', 'w').syswrite(good_csv)
-      @good_csv_file = File.open('good.csv')
+      @good_csv = "mrn,first_name,last_name,dob\r\n9988101,,,\r\n9988102,,,\r\n,test10,last10,12/31/04\r\n,test11,last11,6/11/54\r\n,test12,last12,6/12/54\r\n,test13,last13,6/13/55\r\n,test14,last14,7/26/74\r\n,test15,last15,12/31/04\r\n,test16,last16,6/12/54\r\n"
+      @bad_csv = "mrn,first_name,last_name,dob\r\n9988101,,,\r\nfoo,,,\r\n,,last10,9/1/45\r\n,test11,last11,6/1/10\r\n,test12,last12,13/31/01\r\n,,,6/13/55\r\n,test14,last14,7/26/74\r\n,test15,last15,12/31/04\r\n,test16,,6/12/54\r\n"
+      File.new('good.csv', 'w').syswrite(@good_csv)
+      @good_csv_file = File.open('good.csv', 'r')
+      File.new('bad.csv', 'w').syswrite(@bad_csv)
+      @bad_csv_file = File.open('bad.csv', 'r')
       controller.stub!(:user_must_be_logged_in)
     end
     it "should allow me to post the file to create" do
+      controller.stub!(:check_csv).and_return(true)
       post :create, {:file => @good_csv_file}
       response.should be_success
     end
+    it "should check the file (success) and redirect to study" do
+      controller.should_receive(:check_csv).with(@good_csv_file).and_return(true)
+      post :create, {:file => @good_csv_file, :study => 3}
+      response.should redirect_to(study_path(:id => 3))
+    end
+    it "should check the file (failure) and send me back a csv file" do
+      controller.should_receive(:check_csv).with(@bad_csv_file).and_return(false)
+      post :create, {:file => @bad_csv_file, :study => 3}
+      response.headers['Content-Type'].include?("text/csv").should be_true
+    end
+    it "should check csv for validity" do
+      controller.class.check_csv(@good_csv_file).should be_true
+      controller.class.check_csv(@bad_csv_file).should be_false
+    end
+    
+
+    
   end
 end
