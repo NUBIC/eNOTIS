81d83f6ff59713efa1b0680c17e4a9a7f77409c2
Mark Yoon <yoon@northwestern.edu>
2009-06-16 10:19:47 -0500

    added haml-edge, compass gems

--- patient_tracker/config/environment.rb
+++ patient_tracker/config/environment.rb
@@ -18,24 +18,24 @@ Rails::Initializer.run do |config|
 
   # all environments gems
   # For lib/gem loading errors, see http://www.webficient.com/2008/07/11/ruby-on-rails-gem-dependencies-and-plugin-errors
-
+  
+  # config.gem "haml-edge", :version => '2.1.24', :lib => false # required by chriseppstein-compass
+  # config.gem "chriseppstein-compass", :version => '0.6.15', :lib => false, :source => "http://gems.github.com"
   # config.gem "haml", :version => '2.0.9', :source => "http://gems.rubyforge.org/"
   # # config.gem "httpclient", :version => '2.1.5'
   # config.gem "sqlite3-ruby", :version => '1.2.4', :lib => "sqlite3"
-  # config.gem "soap4r", :version => '1.5.8', :lib => false # :lib => false fixes no such file to load -- soap4r
+  # config.gem "soap4r", :version => '1.5.8', :lib => false # :lib => false fixes no such file to load
   # config.gem "libxml-ruby", :version => '1.1.3', :lib => false # :lib => false fixes no such file to load
-  # config.gem "thoughtbot-factory_girl", :version => '1.2.1', :lib    => "factory_girl", :source => "http://gems.github.com"
+  # config.gem "thoughtbot-factory_girl", :version => '1.2.1', :lib => "factory_girl", :source => "http://gems.github.com"
   # config.gem "populator", :version => '0.2.5'
   # config.gem "faker", :version => '0.3.1'
   # config.gem "mechanize", :version => '0.9.2' # also installs nokogiri
   # # config.gem "mechanize-ntlm", :version => '0.9.1'   # download from http://www.mindflowsolutions.net/2009/5/21/ruby-ntlm-mechanize and run sudo gem install mechanize-ntlm-0.9.1
   
-
-  
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
   # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
-
+    
   # Skip frameworks you're not going to use. To use Rails without a database,
   # you must remove the Active Record framework.
   # config.frameworks -= [ :active_record, :active_resource, :action_mailer ]
--- patient_tracker/config/environments/test.rb
+++ patient_tracker/config/environments/test.rb
@@ -29,9 +29,9 @@ config.action_mailer.delivery_method = :test
 
 # test environment gems
 
-# config.gem "webrat", :version => '0.4.4'
 # config.gem "rspec", :version => '1.2.6', :lib => false
 # config.gem "rspec-rails", :version => '1.2.6', :lib => false
-# config.gem "rcov", :version => '0.8.1.2.0'
-# config.gem "cucumber", :version => '0.3.11' # also installs dependencies: term-ansicolor, polyglot, treetop, diff-lcs, builder
-# config.gem "ZenTest", :version => '4.1.1'
+# config.gem "webrat", :version => '0.4.4', :lib => false
+# config.gem "cucumber", :version => '0.3.11', :lib => false # also installs dependencies: term-ansicolor, polyglot, treetop, diff-lcs, builder
+# # config.gem "rcov", :version => '0.8.1.2.0'
+# # config.gem "ZenTest", :version => '4.1.1'
