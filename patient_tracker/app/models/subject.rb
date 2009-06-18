810bb142c58054ba04651368014c23f903806010
Mark Yoon <yoon@northwestern.edu>
2009-06-18 16:21:36 -0500

    document sync in the subject model

--- patient_tracker/app/models/subject.rb
+++ patient_tracker/app/models/subject.rb
@@ -1,12 +1,10 @@
-# Represents a subject that has (at some point) been on a clinical trial study
-# We store enough data to reconcile the local db store record with the subject data in 
-# the EDW. The model stores the reconciliation fields and information about the source system
-# for the data.
+# Represents a subject that has (possibly) been part of a clinical research study.
+# We store enough data to sync the local db record with the subject data in the EDW.
+# The model stores the fields and information about the source system for the data.
 require 'lib/webservices/webservices'
-class Subject < ActiveRecord::Base #.extend WebServices
+class Subject < ActiveRecord::Base
   include WebServices
   has_many :involvements
-  has_many :subject_events 
   has_many :studies, :through => :involvements
 
   $plugins = [EdwServices]
@@ -19,7 +17,7 @@ class Subject < ActiveRecord::Base #.extend WebServices
   end
   def sync!(attrs)
     self.attributes = attrs
-    # self.chagnes from http://ryandaigle.com/articles/2008/3/31/what-s-new-in-edge-rails-dirty-objects
+    # self.changes via http://ryandaigle.com/articles/2008/3/31/what-s-new-in-edge-rails-dirty-objects
     self.pre_sync_data = self.changes.map{|key, array_value| "#{key} changed from #{array_value[0].to_s} to #{array_value[1].to_s}"}.join(",0") unless synced?
     self.save
   end
