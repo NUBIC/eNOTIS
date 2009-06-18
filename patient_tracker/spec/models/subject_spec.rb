ae1c4798db86ba7f792347814e652fc0e059e906
Mark Yoon <yoon@northwestern.edu>
2009-06-18 13:13:46 -0500

    added spec and code for basic subject sync - used factory. closes #1179

--- patient_tracker/spec/models/subject_spec.rb
+++ patient_tracker/spec/models/subject_spec.rb
@@ -2,11 +2,27 @@ require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe Subject do
   before(:each) do
-    @valid_attributes = {
-    }
   end
 
   it "should create a new instance given valid attributes" do
-    Subject.create!(@valid_attributes)
+    Factory(:subject).should be_valid
   end
+  
+  describe "syncing Subject with EDW" do
+    # possible scenarios
+    #   1. EDW down
+    #   2. record not inserted in EDW yet (nightly work)
+    #   3. local user only - pt will never be in EDW (no medical record) 
+
+    it "should tell us when the subject is not synced" do
+      @subject = Factory(:subject, :last_synced => nil)
+      @subject.synced?.should be_false
+    end
+    
+    
+    
+  end
+  
+  
+  
 end
