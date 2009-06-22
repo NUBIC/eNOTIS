560ecf6413d1ca1ac75c526bb1e5988efeac6f8d
Brian Chamberlain <b-chamberlain@northwestern.edu>
2009-06-22 10:27:36 -0500

    updated coordinations table model

--- patient_tracker/db/migrate/20090618205911_create_coordinations.rb
+++ patient_tracker/db/migrate/20090618205911_create_coordinations.rb
@@ -1,7 +1,8 @@
 class CreateCoordinations < ActiveRecord::Migration
   def self.up
     create_table :coordinations do |t|
-
+      t.integer :study_id
+      t.integer :user_id
       t.timestamps
     end
   end
