5965c727212535097d30a6e76eb1cc46aa8bad2c
Brian Chamberlain <b-chamberlain@northwestern.edu>
2009-06-22 10:21:02 -0500

    some formatting and file cleanup

--- patient_tracker/app/models/subject.rb
+++ patient_tracker/app/models/subject.rb
@@ -1,9 +1,12 @@
+require 'lib/webservices/webservices'
+
 # Represents a subject that has (possibly) been part of a clinical research study.
 # We store enough data to sync the local db record with the subject data in the EDW.
 # The model stores the fields and information about the source system for the data.
-require 'lib/webservices/webservices'
+
 class Subject < ActiveRecord::Base
   include WebServices
+
   has_many :involvements
   has_many :studies, :through => :involvements
 
@@ -12,13 +15,16 @@ class Subject < ActiveRecord::Base
   def synced?
     !self.synced_at.nil?
   end
+
   def stale?
     synced? ? self.synced_at < 12.hours.ago : true 
   end
+
   def sync!(attrs)
     self.attributes = attrs
     # self.changes via http://ryandaigle.com/articles/2008/3/31/what-s-new-in-edge-rails-dirty-objects
     self.pre_sync_data = self.changes.map{|key, array_value| "#{key} changed from #{array_value[0].to_s} to #{array_value[1].to_s}"}.join(",0") unless synced?
     self.save
   end
+
 end
