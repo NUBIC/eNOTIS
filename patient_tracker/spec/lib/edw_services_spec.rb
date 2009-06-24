7ac9523c0719e91211940a28a32d35c79d1a57ec
kumbafu <mutero_d@yahoo.com>
2009-06-24 14:13:44 -0500

    added a few tests for edw/eirb translator methods

--- patient_tracker/spec/lib/edw_services_spec.rb
+++ patient_tracker/spec/lib/edw_services_spec.rb
@@ -33,7 +33,20 @@ describe EdwServices do
         @adapter.should_receive(:perform_search).with("e-NOTIS+Test+2",{:first_nm => 'July', :last_nm => 'Fourth', :birth_dts => '7/4/50'})
         EdwServices.find_by_name_and_dob(:first_name => 'July', :last_name => 'Fourth', :birth_date => '7/4/50')
       end
-    end 
+    end
+
+   describe "converting attributes from provided hashes" do
+      it "should provide the converted attribute for each attribute provided that exists in the translator" do
+         @converter = {"attribute1"=>"attribute1_converted","attribute2"=>"attribute2_converted"}
+         EdwServices.convert([{"attribute1"=>"test1","attribute2"=>"test2"}],@converter).should == [{:attribute1_converted=>"test1",:attribute2_converted=>"test2"}]
+      end
+
+      it "should ignore any attribute that doesn't exist in the translator" do
+         @converter = {"attribute1"=>"attribute1_converted","attribute2"=>"attribute2_converted"}
+         EdwServices.convert([{"attribut"=>"test1","attribute2"=>"test2"}],@converter).should == [{:attribute2_converted=>"test2"}]
+      
+      end 
+   end 
   
     it "always connects" do
       EdwServices.should_receive(:connect)
