05909667fb1466e71251cb96adcaa9739805d56f
Mark Yoon <yoon@northwestern.edu>
2009-06-30 16:33:19 -0500

    working on studies and dashboard views

--- patient_tracker/app/controllers/studies_controller.rb
+++ patient_tracker/app/controllers/studies_controller.rb
@@ -4,13 +4,10 @@ class StudiesController < ApplicationController
 
   def index
     @studies = Study.all || []
-    # @studies = Study.find_by_coordinator(params[:net_id])
-    # @studies = current_user ? current_user.studies : []
   end
 	
   def show
-   @study = Study.find_by_id(params[:id])
-   @subjects = Involvement.find_by_study_id(@study)
+    @study = Study.find_by_id(params[:id])
   end
 	
   def search
