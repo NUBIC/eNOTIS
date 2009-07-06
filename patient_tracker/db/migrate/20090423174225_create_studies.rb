3b9fff52effafad935033dfffea463d3e01834f1
Brian Chamberlain <b-chamberlain@northwestern.edu>
2009-07-06 14:05:18 -0500

    Added new study fields to hold pi and sc data

--- patient_tracker/db/migrate/20090423174225_create_studies.rb
+++ patient_tracker/db/migrate/20090423174225_create_studies.rb
@@ -5,8 +5,18 @@ class CreateStudies < ActiveRecord::Migration
       t.string :name
       t.string :title
       t.string :phase
+      t.string :research_type
       t.string :description
       t.string :status
+      t.string :pi_netid
+      t.string :pi_first_name
+      t.string :pi_last_name
+      t.string :pi_email
+      t.string :sc_netid
+      t.string :sc_first_name
+      t.string :sc_last_name
+      t.string :sc_email
+
       t.datetime :synced_at
       t.timestamps
     end
