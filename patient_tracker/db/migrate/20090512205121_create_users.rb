e0079792a6b9b3ac5ba62921e1c2695b96787b9c
Brian Chamberlain <b-chamberlain@northwestern.edu>
2009-07-07 14:50:53 -0500

    added the correct email field to the translation dictionary. Updated the model to reflect reorganization

--- patient_tracker/db/migrate/20090512205121_create_users.rb
+++ patient_tracker/db/migrate/20090512205121_create_users.rb
@@ -3,13 +3,11 @@ class CreateUsers < ActiveRecord::Migration
     create_table :users do |t|
       t.string :netid
       t.string :email
-      t.string :email_type
 
       t.string :title
       t.string :first_name
       t.string :last_name
       t.string :middle_name
-      t.string :description
 
       t.string :address_line1
       t.string :address_line2
@@ -18,22 +16,10 @@ class CreateUsers < ActiveRecord::Migration
       t.string :state
       t.string :postal_code
       t.string :country
-      t.string :address_description
-      t.string :address_type
 
-      t.string :training_id
-      t.string :training_type_id
-      t.string :training_type
-      t.string :training_certification_date
-      t.string :training_expiration_date
-
-      t.string :phone_description
       t.string :phone_number
-      t.string :phone_type
 
-      t.string :fax_description
       t.string :fax_number
-      t.string :fax_type
       
       t.datetime :eirb_create_date
       
