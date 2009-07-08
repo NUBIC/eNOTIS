73f6dc00fbb437153889caa19a9b4e7c3188bdfc
Mark Yoon <yoon@northwestern.edu>
2009-07-08 10:51:54 -0500

    add may_accrue? method to study model

--- patient_tracker/app/models/study.rb
+++ patient_tracker/app/models/study.rb
@@ -5,6 +5,49 @@ require 'lib/webservices/webservices'
 # about the study (Title, PI, Description, Approval Date, etc) most of the data
 # is pulled from the EDW (from the eIRB db export) as needed.
 
+# Possible eIRB statuses
+# 
+# Approval Correspondence Review
+# Approved
+# Assigned to Meeting
+# Assigned to Meeting: Changes Requested
+# Awaiting Approval Correspondence
+# Awaiting Coordinator Assignment
+# Awaiting Meeting Assignment
+# Chair Correspondence Review
+# Changes Requested by Dept Reviewer
+# Changes Requested By OPRS Staff
+# Changes Required by IRB
+# Closed/Terminated
+# Completed
+# Conditional Approval
+# Correspondence Review: PCH or DR
+# Dept Site Anc Review
+# Designated Reviewer Conditions Review
+# Exempt Approval Revoked
+# Exempt Approved
+# Exempt Correspondence Review
+# Exempt Review: Awaiting Approval Correspondence
+# Exempt Review: Awaiting Correspondence
+# Exempt Review: Changes Requested
+# Expedited Review: Awaiting Correspondence
+# Expedited Review: Changes Requested
+# Expired
+# Expired: Periodic Review In Progress
+# In Exempt Review
+# In Expedited Review
+# Meeting Complete Awaiting Correspondence
+# Not Under IRB Purview
+# OPRS Staff Conditions Review
+# OPRS Staff Review
+# Original Version
+# Pre Submission
+# Rejected
+# Revision Closed
+# Revision Open
+# Suspended
+# Withdrawn
+
 class Study < ActiveRecord::Base
 	has_many :involvements
   has_many :coordinators
@@ -38,6 +81,10 @@ class Study < ActiveRecord::Base
   end
   def documents
     []
+  end
+  def may_accrue?
+    # returns false if self.status.nil?
+    ["Approved", "Conditional Approval", "Exempt Approved", "Not Under IRB Purview", "Revision Open"].include? self.status
   end
   
 end
