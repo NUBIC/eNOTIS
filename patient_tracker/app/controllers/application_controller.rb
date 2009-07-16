a3b6281f3db6088daaa659dbda7210bb76764c3e
Mark Yoon <yoon@northwestern.edu>
2009-07-16 09:16:28 -0500

    clean up system status messages, don't show login for red statuses

--- patient_tracker/app/controllers/application_controller.rb
+++ patient_tracker/app/controllers/application_controller.rb
@@ -15,9 +15,9 @@ class ApplicationController < ActionController::Base
   APP_VERSION = "0.0.1a"
   
   SYSTEM_STATUSES = { :up => "Don't think twice, it's alright",
-                      :down => "Blowin' in the wind (down, we're working on it)",
-                      :scheduled_maintenance => "The times they are a-changin' (down for scheduled maintenance)",
-                      :scheduled_restored => "Like a rolling stone (scheduled maintenance complete)" }
+                      :down => "Blowin' in the wind<br/>(down, we're working on it)",
+                      :scheduled_maintenance => "The times they are a-changin'<br/>(down for scheduled maintenance)",
+                      :scheduled_restored => "Like a rolling stone<br/>(scheduled maintenance complete)" }
   
   def system_status
     %w(up down scheduled_maintenance scheduled_restored).rand
