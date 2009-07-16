d023c101794b05b6d6de468da900b7e2b5848e38
Brian Chamberlain <b-chamberlain@northwestern.edu>
2009-07-16 17:29:36 -0500

    forgot to save this file before commiting

--- patient_tracker/db/migrate/20090716210120_create_terms.rb
+++ patient_tracker/db/migrate/20090716210120_create_terms.rb
@@ -1,7 +1,11 @@
 class CreateTerms < ActiveRecord::Migration
   def self.up
     create_table :terms do |t|
-
+      t.string :term
+      t.string :code
+      t.string :type
+      t.string :source
+      t.text :description
       t.timestamps
     end
   end
