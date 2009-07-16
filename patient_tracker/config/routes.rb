139e30cfe524fc44d089ad980de91df390bb5d77
Mark Yoon <yoon@northwestern.edu>
2009-07-16 15:02:15 -0500

    access help on login page

--- patient_tracker/config/routes.rb
+++ patient_tracker/config/routes.rb
@@ -1,5 +1,5 @@
 ActionController::Routing::Routes.draw do |map|
-  map.resources :authentication, :collection => {:login => :post, :logout => :any}
+  map.resources :authentication, :collection => {:login => :post, :logout => :any, :access_help => :get}
   map.resources :studies
   map.resources :subjects, :collection => {:search=>:get,:sync=>:post}
   map.resources :users, :collection => {:dashbaord => :get}
