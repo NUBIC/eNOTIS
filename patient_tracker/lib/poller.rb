8d2a9fae00df7a13673926326a592a4f10674534
Brian Chamberlain <b-chamberlain@northwestern.edu>
2009-07-16 17:21:26 -0500

    Fix for the poller script and cleaned up the lib module

--- patient_tracker/lib/poller.rb
+++ patient_tracker/lib/poller.rb
@@ -3,19 +3,11 @@
 STDOUT.sync = true; STDOUT.flush
 STDERR.sync = true; STDERR.flush
 
-#Try to Load Merb
-merb_init_file = File.expand_path(File.dirname(__FILE__)+'/../config/merb_init')
-if File.exists? merb_init_file
-  require File.expand_path(File.dirname(__FILE__)+'/../config/boot')
-  #need this because of the CWD
-  Merb.root = MERB_ROOT
-  require merb_init_file
-else
+
   # Load Rails
   RAILS_ROOT=File.expand_path(File.join(File.dirname(__FILE__), '..'))
-  require File.join(RAILS_ROOT, 'config', 'boot')
   require File.join(RAILS_ROOT, 'config', 'environment')
-end
+
 
 # Load ActiveMessaging processors
 #ActiveMessaging::load_processors
