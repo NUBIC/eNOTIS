d206a23eb6316a1d7cf8c4668464c3a81d90cd5c
Brian Chamberlain <b-chamberlain@northwestern.edu>
2009-07-17 15:59:38 -0500

    fix involvement table column problem

--- patient_tracker/db/migrate/20090716200101_create_races.rb
+++ patient_tracker/db/migrate/20090716200101_create_races.rb
@@ -1,13 +1,16 @@
 class CreateRaces < ActiveRecord::Migration
   def self.up
     create_table :races do |t|
+      t.integer :involvement_id
       t.integer :race_type_id
       t.timestamps
     end
+    add_index(:races, :involvement_id)
     add_index(:races, :race_type_id)
   end
 
   def self.down
+    remove_index(:races, :involvement_id)
     remove_index(:races, :race_type_id)
     drop_table :races
   end
