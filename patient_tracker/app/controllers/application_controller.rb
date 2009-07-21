840591ef661e8512e4cc01bfcc13717a7000562c
Brian Chamberlain <b-chamberlain@northwestern.edu>
2009-07-21 12:13:48 -0500

    Updated controllers and views to use new sass files and haml layouts

--- patient_tracker/app/controllers/application_controller.rb
+++ patient_tracker/app/controllers/application_controller.rb
@@ -8,9 +8,8 @@ class ApplicationController < ActionController::Base
   helper :all # include all helpers, all the time
   protect_from_forgery # See ActionController::RequestForgeryProtection for details
 
-  # TODO Turn this back on -blc
   # Scrub sensitive parameters from your log
-  # filter_parameter_logging :password
+  filter_parameter_logging :password
 
   APP_VERSION = "0.0.1a"
   
@@ -18,8 +17,9 @@ class ApplicationController < ActionController::Base
                       :down => "Blowin' in the wind<br/>(down, we're working on it)",
                       :scheduled_maintenance => "The times they are a-changin'<br/>(down for scheduled maintenance)",
                       :scheduled_restored => "Like a rolling stone<br/>(scheduled maintenance complete)" }
+
   
   def system_status
-    %w(up down scheduled_maintenance scheduled_restored).rand
+    %w(up up up up up up up up up up up up up down scheduled_maintenance scheduled_restored).rand
   end
 end
--- patient_tracker/app/controllers/subjects_controller.rb
+++ patient_tracker/app/controllers/subjects_controller.rb
@@ -1,13 +1,40 @@
+require 'fastercsv'
+require 'activemessaging/processor'
+
 class SubjectsController < ApplicationController
-  require 'fastercsv'
-  require 'activemessaging/processor'
+  layout "layouts/main"  
+
+  # Includes
   include ActiveMessaging::MessageSender
   include FaceboxRender
+
+  # Filters
   before_filter :user_must_be_logged_in
-  layout "main"
+
+  # Auditing
   publishes_to :patient_upload
   has_view_trail :except => :index
+
+
+  # =================== Class Methods =========================
   
+  def self.csv_sanity_check(csv, temp_file = Tempfile.new("results")) # csv can be a string, file, or Paperclip::Attachment
+    # We may possibly want to sanity check dates with Chronic http://chronic.rubyforge.org/
+    csv = csv.to_io if csv.class == Paperclip::Attachment
+    errors = []
+    FasterCSV.open(temp_file.path, "r+") do |temp_stream|
+      # TODO This is unacceptable. Please clean it up
+      FasterCSV.parse(csv, :headers => :first_row, :return_headers => true, :header_converters => :symbol) do |r|      
+        errors << (r[:mrn].blank? ? (r[:last_name].blank? or r[:first_name].blank? or r[:birth_date].blank?) ? "A first_name and last_name and birth_date, or an mrn is required. " : "" : "") + \
+          ((r[:subject_event_type].blank? or r[:subject_event_date].blank?) ? "A subject event type and date is required." : "")
+        temp_stream << (errors.size == 1 ? ["import_errors"] : [errors.last]) + r.fields
+      end
+    end
+    errors.uniq == [""]
+  end
+
+  # ===================== Public Actions ======================
+
   def index
     if params[:irb_number]
       @study = Study.find_by_irb_number(params[:irb_number])
@@ -75,18 +102,4 @@ class SubjectsController < ApplicationController
     end
   end
   
-  def self.csv_sanity_check(csv, temp_file = Tempfile.new("results")) # csv can be a string, file, or Paperclip::Attachment
-    # We may possibly want to sanity check dates with Chronic http://chronic.rubyforge.org/
-    csv = csv.to_io if csv.class == Paperclip::Attachment
-    errors = []
-    FasterCSV.open(temp_file.path, "r+") do |temp_stream|
-      # TODO This is unacceptable. Please clean it up
-      FasterCSV.parse(csv, :headers => :first_row, :return_headers => true, :header_converters => :symbol) do |r|      
-        errors << (r[:mrn].blank? ? (r[:last_name].blank? or r[:first_name].blank? or r[:birth_date].blank?) ? "A first_name and last_name and birth_date, or an mrn is required. " : "" : "") + \
-          ((r[:subject_event_type].blank? or r[:subject_event_date].blank?) ? "A subject event type and date is required." : "")
-        temp_stream << (errors.size == 1 ? ["import_errors"] : [errors.last]) + r.fields
-      end
-    end
-    errors.uniq == [""]
-  end
-end
+ end
