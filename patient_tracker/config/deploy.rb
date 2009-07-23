8677df5c9d65763fb7ec913d28e0ac61e3bcca1c
Mark Yoon <yoon@northwestern.edu>
2009-07-23 16:45:55 -0500

    working on deploy

--- patient_tracker/config/deploy.rb
+++ patient_tracker/config/deploy.rb
@@ -1,7 +1,3 @@
-# http://github.com/guides/deploying-with-capistrano
-# http://www.brynary.com/2008/8/3/our-git-deployment-workflow
-# http://weblog.jamisbuck.org/2007/7/23/capistrano-multistage
-
 set :application, "enotis"
 
 # Version control
@@ -14,12 +10,31 @@ set :deploy_via, :remote_cache
 
 # User
 set :user, "enotis-deployer"
+set :use_sudo, false
+set :db_user, "enotis"
+set :db_password, ""
 
 # Roles
 task :set_roles do
   role :app, app_server
   role :web, app_server
   role :db, app_server, :primary => true
+end
+
+# Staging environment
+desc "Deploy to staging"
+task :staging do
+  set :app_server, "enotis-staging.nubic.northwestern.edu"
+  set :rails_env, "staging"
+  set_roles
+end
+
+# Production environment
+desc "Deploy to production"
+task :production do
+  set :app_server, "enotis.nubic.northwestern.edu"
+  set :rails_env, "production"
+  set_roles
 end
 
 # Deploy start/stop/restart
@@ -34,16 +49,54 @@ namespace :deploy do
   end
 end
 
-# Staging
-desc "deploy to staging"
-task :staging do
-  set :app_server, "enotis-staging.nubic.northwestern.edu"
-  set_roles
+# Inspiration
+# http://github.com/guides/deploying-with-capistrano
+# http://www.brynary.com/2008/8/3/our-git-deployment-workflow
+# http://weblog.jamisbuck.org/2007/7/23/capistrano-multistage
+
+# Gems
+# http://henrik.nyh.se/2008/10/cap-gems-install-and-a-gem-dependency-gotcha
+namespace :gems do
+  desc "Install gems"
+  task :install, :roles => :app do
+    # always use sudo
+    run "cd #{current_path} && sudo rake RAILS_ENV=#{rails_env} gems:install"
+  end
 end
 
-# Production
-desc "deploy to production"
-task :production do
-  set :app_server, "enotis.nubic.northwestern.edu"
-  set_roles
-end
+# Dynamic database.yml generation
+# http://shanesbrain.net/2007/5/30/managing-database-yml-with-capistrano-2-0
+# require 'erb'
+# 
+# before "deploy:setup", :db
+# after "deploy:update_code", "db:symlink" 
+# 
+# namespace :db do
+#   desc "Create database yaml in shared path" 
+#   task :default do
+#     db_config = ERB.new <<-EOF
+#     base: &base
+#       adapter: oracle
+#       username: #{db_user}
+#       password: #{db_password}
+# 
+#     staging:
+#       adapter: sqlite3
+#       database: db/production.sqlite3
+#       pool: 5
+#       timeout: 5000
+#     
+#     production:
+#       database: nubic_#{application}
+#       <<: *base
+#     EOF
+# 
+#     run "mkdir -p #{shared_path}/config" 
+#     put db_config.result, "#{shared_path}/config/database.yml" 
+#   end
+# 
+#   desc "Make symlink for database yaml" 
+#   task :symlink do
+#     run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml" 
+#   end
+# end
