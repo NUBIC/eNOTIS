c060f642236e2f7f63e2c73d1f65f154c61c1b41
yoon@northwestern.edu <yoon@yoons-imac.cable.rcn.com>
2009-12-03 13:44:14 -0600

    new columns and tooltip for studies in dashboard

--- app/controllers/studies_controller.rb
+++ app/controllers/studies_controller.rb
@@ -16,16 +16,17 @@ class StudiesController < ApplicationController
       end
       # See http://datatables.net/forums/comments.php?DiscussionID=53 for json params
       format.json do
-        query_cols = %w(irb_number name status)
-        cols = %w(irb_number name status accrual)
+        query_cols = %w(irb_number pi_last_name)
+        cols = %w(irb_number pi_last_name accrual)
         q = "%#{params[:sSearch]}%"
         order = (1..(params[:iSortingCols].to_i)).map{|i| [cols[(params["iSortCol_#{i-1}".to_sym].to_i || 0)], (params["iSortDir_#{i-1}"] || "ASC")].join(" ")}.join(",")
-        results = Study.find( :all,
+        studies = Study.find( :all,
                               :select => "*, (SELECT COUNT(*) FROM involvements WHERE study_id = studies.id) AS accrual",
                               :offset => params[:iDisplayStart] || 0,
                               :limit => params[:iDisplayLength] || 10,
                               :order => order,
-                              :conditions => [query_cols.map{|x| "#{x} LIKE ?"}.join(" OR "), q,q,q]).map{|s| cols.map{|col| (col == "irb_number" ? "<a href='#{study_path(s)}'>#{s.send(col)}</a>" : s.send(col))} }
+                              :conditions => [query_cols.map{|x| "#{x} LIKE ?"}.join(" OR "), q,q])
+        results = studies.map{|s| cols.map{|col| (col == "irb_number" ? "#{s.may_accrue? ? "O" : "X"} <a href='#{study_path(s)}' title='#{s.title}'>#{s.send(col)}</a>" : s.send(col))} }
         render :json => {:aaData => results, :iTotalRecords => results.size, :iTotalDisplayRecords => Study.count}
       end
     end
