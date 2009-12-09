c2f935557ad26fe46971a7a3642360e389bad352
Mark Yoon <yoon@northwestern.edu>
2009-12-09 17:21:01 -0600

    gem dependency adjustments for bcsec, bcdatabase, sources

--- config/environment.rb
+++ config/environment.rb
@@ -20,13 +20,14 @@ Rails::Initializer.run do |config|
   # For lib/gem loading errors, see http://www.webficient.com/2008/07/11/ruby-on-rails-gem-dependencies-and-plugin-errors
   
   config.gem "haml", :version => '>=2.2.0', :lib => false # required by chriseppstein-compass
-  config.gem "chriseppstein-compass", :version => '>=0.8.13', :lib => false, :source => "http://gems.github.com"
+  config.gem "chriseppstein-compass", :version => '>=0.8.13', :lib => false
   config.gem "soap4r", :version => '>=1.5.8', :lib => false # :lib => false fixes no such file to load
   config.gem "libxml-ruby", :version => '>=1.1.3', :lib => false # :lib => false fixes no such file to load
   config.gem "fastercsv", :version => '>=1.5.0'
   config.gem "paperclip", :version => '>=2.1.2'
-  config.gem "bcsec", :source => "http://download.bioinformatics.northwestern.edu/gems/" # also installs bcdatabase
-  config.gem "airblade-paper_trail", :lib => 'paper_trail', :version => '>=1.1.1', :source => 'http://gems.github.com'
+  config.gem "bcdatabase", :version => '<1.0.0'
+  config.gem "bcsec" #, :source => "http://download.bioinformatics.northwestern.edu/gems/"
+  config.gem "airblade-paper_trail", :lib => 'paper_trail', :version => '>=1.1.1'
   config.gem "chronic", :version => '>=0.2.3'
   config.gem "stomp", :version => '>=1.1'
   config.gem "binarylogic-searchlogic", :lib => 'searchlogic'
