16dbb4d2d2baba236c25c88026482a125441d90d
Mark Yoon <yoon@northwestern.edu>
2009-12-11 13:35:58 -0600

    cleanup

--- public/javascripts/jquery.main.js
+++ public/javascripts/jquery.main.js
@@ -1,29 +1,19 @@
 $(document).ready(function() {
-  // if the function argument is given to overlay, 
-  // it is assumed to be the onBeforeLoad event listener 
+  // overlay  
   $("a[rel=#overlay]").overlay({ 
     onBeforeLoad: function() { var wrap = this.getContent().find(".contentWrap"); wrap.load(this.getTrigger().attr("href"), "format=js"); },
     expose: { color: '#fff', loadSpeed: 200, opacity: 0.5 }
   }); 
-
-	jQuery('input[name="query"]').liveSearch({url: '/search?format=js&query=', id: "livesearch-results"});
-	
-	// save the width of the flash since changing it from absolute positioning at 40% to relative positioning (to slide up) decreases the width
-	var flashWidth = $("#flash").css('width');
+  // livesearch
+	$('input[name="query"]').liveSearch({url: '/search?format=js&query=', id: "livesearch-results"});
+	// flash messages
+	var flashWidth = $("#flash").css('width'); // save flash width. changing it to relative positioning (to slide up) decreases width
 	$("#flash").animate({width: flashWidth}, 3000).slideUp(1000);
-	
-	// http://net.tutsplus.com/javascript-ajax/how-to-create-a-keypress-navigation-using-jquery/
-	$(document).keypress(function(e)
-  	{
-      // alert(e.which);
-  		switch(e.which)
-  		{
-  			// user presses the "/"
-  			case 47:	
-  			      $('#query').focus();
-  			      e.stopImmediatePropagation();
-  						break;	
-  		}
-  	});
-  
-})
+	// shortcut keys - http://net.tutsplus.com/javascript-ajax/how-to-create-a-keypress-navigation-using-jquery
+	$(document).keypress(function(e) { // alert(e.which);
+    switch(e.which){
+			case 47: // user presses "/"
+	      $('#query').focus(); break;	
+		}
+	});
+});
