c566d1c27ce9d789629821588f69276fd9de94f0
Mark Yoon <yoon@northwestern.edu>
2009-12-15 14:52:17 -0600

    javascript consolidation

--- public/javascripts/jquery.main.js
+++ public/javascripts/jquery.main.js
@@ -9,4 +9,24 @@ $(document).ready(function() {
 	// flash messages
 	var flashWidth = $("#flash").css('width'); // save flash width. changing it to relative positioning (to slide up) decreases width
 	$("#flash").animate({width: flashWidth}, 5000).slideUp(1000);
+	
+	// studies index
+	// tooltip
+  $("#my_studies a.study").tooltip({position: 'bottom right', predelay: 800});
+  // dataTable
+  // $("#study_list .display").dataTable({"sPaginationType": "full_numbers",bServerSide: true, sAjaxSource: '/studies.json' });
+  $("#my_studies .display").dataTable({"sPaginationType": "full_numbers","aoColumns": [{ "sType": "html" }, null, null]});
+	
+	// studies show
+	// overlay
+  $("a[rel=#overlay]").overlay({expose: { color: '#fff', loadSpeed: 200, opacity: 0.5 }});
+  $("a[rel=#import]").overlay({expose: { color: '#fff', loadSpeed: 200, opacity: 0.5 }});
+  $('#overlay').css({'width': '955px'});
+  // tooltip
+  $("#study a.study").tooltip({position: 'bottom left', offset: [0, 955], predelay: 800});
+  $("#accrual a.involvement").tooltip({position: 'bottom right', predelay: 800});
+  $("#accrual a.subject").tooltip({position: 'bottom right', predelay: 800});
+  // dataTable
+  $("#accrual .display").dataTable({"sPaginationType": "full_numbers","aoColumns": [{ "sType": "html" },{ "sType": "html" },{ "sType": "html" },{ "sType": "html" }]});
+	
 });
