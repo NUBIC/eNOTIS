1427cd88dd9e734bd1a4b977d99ecd9222f26803
yoon@northwestern.edu <yoon@yoons-imac.(none)>
2010-01-07 14:46:33 -0600

    changed array counting

--- lib/array.rb
+++ lib/array.rb
@@ -2,15 +2,19 @@ class Array
   def rand
     self[Kernel.rand(self.length)] 
   end
-  def count_all(*attributes)
-    result = {}
-    attributes.map do |attribute|
-      result[attribute] = self.inject({}) do |hash, item|
-        hash[item[attribute].to_s.to_sym] ||= 0
-        hash[item[attribute].to_s.to_sym] += 1
+  def count_all(attribute, subattribute=nil)
+    [[attribute.to_s, 'quantity']] + self.inject({}) do |hash, item|
+      if item.send(attribute).is_a? Array
+        item.send(attribute).each do |x|
+          hash[x.send(subattribute).to_s] ||= 0
+          hash[x.send(subattribute).to_s] += 1
+        end
+        hash
+      else
+        hash[item.send(attribute).to_s] ||= 0
+        hash[item.send(attribute).to_s] += 1
         hash
       end
-    end
-    result
+    end.to_a.sort
   end
 end
--- spec/lib/array_spec.rb
+++ spec/lib/array_spec.rb
@@ -2,14 +2,14 @@ require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 require 'array'
 describe Array do
   before(:each) do
-    @a = [ Factory(:involvement, :gender_type_id => 2, :ethnicity_type_id => 5),
-          Factory(:involvement, :gender_type_id => 2, :ethnicity_type_id => 4),
-          Factory(:involvement, :gender_type_id => 1, :ethnicity_type_id => 3) ]
+    @a = [ Factory(:involvement, :gender_type_id => 2, :ethnicity_type_id => 5, :race_type_ids => [7,8,9]),
+          Factory(:involvement, :gender_type_id => 2, :ethnicity_type_id => 4, :race_type_ids => [7]),
+          Factory(:involvement, :gender_type_id => 1, :ethnicity_type_id => 3, :race_type_ids => [8]) ]
   end
   it "should count attributes" do
-    @a.count_all(:gender_type_id).should == {:gender_type_id => {:"2" => 2, :"1" => 1}}
+    @a.count_all(:gender_type_id).should == [["gender_type_id", "quantity"], ["1",1], ["2",2]]
   end
-  it "should count multiple attributes" do
-    @a.count_all(:ethnicity_type_id, :gender_type_id).should == {:gender_type_id => {:"2" => 2, :"1" => 1}, :ethnicity_type_id => {:"3" => 1, :"4" => 1, :"5" => 1}}
+  it "should count array attributes" do
+    @a.count_all(:races, :race_type_id).should == [["races", "quantity"], ["7",2], ["8",2], ["9", 1]]
   end
 end
