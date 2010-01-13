4040a734c39c13a31a2d90ac0dd4ec70f98f5185
Brian Chamberlain <b-chamberlain@northwestern.edu>
2010-01-12 19:47:13 -0600

    added couchdb alternate storage for eirb data

---
+++ lib/couch_study.rb
@@ -0,0 +1,91 @@
+require 'rubygems'
+require 'couchrest'
+require 'webservices/plugins/eirb_services'
+
+class CouchStudy
+
+  CHILD_DATA = {:pis => "pi",:co_pis => "co_pis",:coords => "coords", :al => "access_list",:pops => "populations", :acrl => "subject_accrual"}
+
+  attr_accessor :db, :studies
+
+  def initialize(db_name)
+    @db = CouchRest.database!(db_name)
+    @studies = []
+  end
+
+  def get_studies
+    @studies = EirbServices.find_basics #finds (almost) all the studies
+  end
+
+  def get_pis
+    EirbServcies.find_principal_investigators
+  end
+
+  def get_co_pis
+    EirbServices.find_co_investigators
+  end
+
+  def get_coords
+    EirbServices.find_coordinators
+  end
+
+  def get_al
+    EirbServices.find_access_list
+  end
+
+  def get_pops
+    EirbServices.find_populations
+  end
+
+  def get_acrl
+    EirbServices.find_accrual
+  end
+
+  def create_studies
+    get_studies
+    form_couch_id
+    @db.bulk_save(@studies)
+  end
+
+  # This does all the work
+  def process
+    create_studies
+    CHILD_DATA.each do |k,v|
+      #preping the instance var
+      data = send("get_#{k}")
+      data.each do |d|
+        append(v,d)
+      end
+    end
+  end
+
+  def append(key,obj)
+    #look in the obj for the irb_number
+    begin
+      doc = @db.get(obj[:irb_number]) #find the doc
+    rescue
+      doc = rescue_save(obj)
+    end
+    doc[key]=obj
+    doc.save
+  end
+
+  # Called if parent not found (forces second call and create)
+  def rescue_save(obj)
+    doc = EirbServices.find_basics(:irb_number => obj[:irb_number])
+    puts "#{obj[:irb_number]} referenced but not found" if doc.empty?
+    d = doc.first
+    hmap!(d)
+    @db.save_doc(d)
+    @db.get(obj[:irb_number])
+  end
+
+  def form_couch_id
+    @studies.map{|x| hmap!(x)}
+  end
+
+  def hmap!(hsh)
+    hsh["_id"]=hsh[:irb_number] if hsh[:irb_number]
+  end
+
+end
