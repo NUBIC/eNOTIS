c39214173df9c463cf7ce23bc11936edc463b612
yoon <yoon@northwestern.edu>
2010-01-21 16:00:54 -0600

    fixing subjects controller spec

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -29,24 +29,10 @@ class SubjectsController < ApplicationController
   
   def create
     # Subjects are created via uploads
-    @study = Study.find_by_irb_number(params[:study_id])
-    @up = StudyUpload.create(:user_id => current_user.id, :study_id => @study.id, :upload => params[:file])
+    @study = Study.find_by_irb_number(params[:study_id]) || Study.new
+    @up = StudyUpload.create(:user => current_user, :study_id => @study.id, :upload => params[:file])
     success = @up.legit?
     redirect_to_studies_or_study(params[:study_id], success ? :notice : :error, @up.summary)
-    # setup_csv(request, headers, @up.upload_file_name.gsub(/(\.csv)?$/, '-result.csv')
-    
-    # temp_file = Tempfile.new("results")
-    # if csv_sanity_check(@study_upload.upload, temp_file)
-    #   @study_upload.save
-    #   temp_file.close!
-    #   publish :patient_upload, @study_upload.id.to_s
-    #   redirect_to_studies_or_study params[:study_id]
-    # else
-    #   @study_upload.result = temp_file
-    #   temp_file.close!
-    #   setup_csv(request, headers, @study_upload.upload_file_name.gsub(/(\.csv)?$/, '-result.csv'))
-    #   render :text => @study_upload.result.to_io.read
-    # end
   end
   
   def search
@@ -66,28 +52,4 @@ class SubjectsController < ApplicationController
     flash[flash_type] = flash_message if !flash_type.blank? && !flash_message.blank?
     redirect_to study_id.blank? ? studies_path : study_path(study_id)
   end
-  def setup_csv(r, h, filename)
-    h['Content-Disposition'] = "attachment; filename=#{filename}"
-    # Internet explorer requires special headers in order for a csv file to be downloaded instead of displayed
-    if r.env['HTTP_USER_AGENT'] =~ /msie/i
-      h.merge!({'Pragma' => 'public', 'Content-type' => 'text/plain; charset=utf-8', 'Cache-Control' => 'no-cache, must-revalidate, post-check=0, pre-check=0', 'Expires' => '0'})
-    else
-      h.merge!({'Content-type' => 'text/csv; charset=utf-8'})
-    end    
-  end
-  def csv_sanity_check(csv, temp_file = Tempfile.new("results")) # csv can be a string, file, or Paperclip::Attachment
-    # We may possibly want to sanity check dates with Chronic http://chronic.rubyforge.org/
-    csv = csv.to_io if csv.class == Paperclip::Attachment
-    csv_is_valid = true
-    FasterCSV.open(temp_file.path, "r+") do |temp_stream|
-      FasterCSV.parse(csv, :headers => :first_row, :return_headers => false, :header_converters => :symbol) do |r|
-        r[:irb_number] = params[:study_id]
-        errors = InvolvementEvent.sanity_check(r)
-        temp_stream << r.fields + [errors.join(". ")] unless errors.empty?
-        csv_is_valid = false if !errors.empty?
-      end
-    end
-    return csv_is_valid
-  end
- end
-
+end
