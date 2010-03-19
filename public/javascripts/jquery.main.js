c810305b1af383dcf2babbf61f06896decf1b5e9
Mark Yoon <yoon@northwestern.edu>
2010-03-19 14:29:00 -0500

    typo - an extra comma in the overlay calls in jquery.main.js. http://amix.dk/blog/post/19388. closes #2294

--- public/javascripts/jquery.main.js
+++ public/javascripts/jquery.main.js
@@ -1,44 +1,45 @@
 $(document).ready(function() {
   // livesearch
-	$('input[name="query"]').liveSearch({url: '/search?format=js&query=', id: "livesearch-results"});
+  $('input[name="query"]').liveSearch({url: '/search?format=js&query=', id: "livesearch-results"});
 
 	// flash messages
-  $("#flash .close").click(function(){$("#flash").fadeOut(300);});
+  $("#flash .close").click(function(){$("#flash").fadeOut(300); return false;});
 	
 	// studies index
-    $("#my_studies a[rel=#study_information]").overlay({ 
-      onBeforeLoad: function() { var wrap = $("#study_information .contentWrap"); wrap.html(this.getTrigger().next('.study_information').html()); },
-      expose: { color: '#fff', loadSpeed: 200, opacity: 0.5 }
-    });
-    // datatables
-    $("#my_studies .display").dataTable({"sPaginationType": "full_numbers","aoColumns": [{ "sType": "html" }, null, null]});
+  $("#my_studies a[rel=#study_information]").overlay({ 
+    onBeforeLoad: function() { var wrap = $("#study_information .contentWrap"); wrap.html(this.getTrigger().next('.study_information').html()); },
+    expose: { color: '#fff', loadSpeed: 200, opacity: 0.5 }
+  });
+  // datatables
+  $("#my_studies .display").dataTable({"sPaginationType": "full_numbers","aoColumns": [{ "sType": "html" }, null, null]});
 	
 	// studies show
     // dataTable
     $("#accrual .display").dataTable({"iDisplayLength": 30, "sPaginationType": "full_numbers", "oLanguage": {"sZeroRecords": "No subjects yet - click 'Add' or 'Import' to get started."},"aoColumns": [null,null,null,null,null,null,null,{ "sType": "html" },{ "sType": "html" }]});
     $("#accrual .display td:empty, #import .display td:empty").html("--");
 
-    // study information
-    $("#study a[rel=#study_information]").click(function(){
-      $('#study_information').slideToggle();
-      $(this).toggleClass('open');
-    });
-
     // import overlay
     $("a[rel=#import]").overlay({
       onBeforeLoad: function(){ $("#import .wrap").load(this.getTrigger().attr("href"), "format=js"); },
-      expose: { color: '#fff', loadSpeed: 200, opacity: 0.5 },
-    });	
+      expose: { color: '#fff', loadSpeed: 200, opacity: 0.5 }
+    });
     
-    // involvement overlay
+    //// involvement overlay
     $("a[rel=#involvement]").overlay({
-      onBeforeLoad: function(){ $("#involvement .wrap").load(this.getTrigger().attr("href"), "format=js"); },
-      expose: { color: '#fff', loadSpeed: 200, opacity: 0.5 },
+     onBeforeLoad: function(){ $("#involvement .wrap").load(this.getTrigger().attr("href"), "format=js"); },
+     expose: { color: '#fff', loadSpeed: 200, opacity: 0.5 }
     });
 
-    // export overlay
+    //// export overlay
     $("a[rel=#export]").overlay({
-      onBeforeLoad: function(){ $("#export .wrap").load(this.getTrigger().attr("href"), "format=js"); },
-      expose: { color: '#fff', loadSpeed: 200, opacity: 0.5 },
+     onBeforeLoad: function(){ $("#export .wrap").load(this.getTrigger().attr("href"), "format=js"); },
+     expose: { color: '#fff', loadSpeed: 200, opacity: 0.5 }
+    });
+    
+    // study information
+    $("#study a.study_information").click(function(){
+      $('#study_information').slideToggle();
+      $(this).toggleClass('open');
+      return false;
     });
 });
