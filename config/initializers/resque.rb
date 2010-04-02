cc7e187996543900b54b2de376e35e90c111f43e
Sam Granieri <sam@samgranieri.com>
2010-04-02 17:24:39 -0500

    quick fix

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,3 +1,4 @@
+require 'yaml'
 rails_root    = ENV['RAILS_ROOT'] || File.dirname(__FILE__) + '/../..'
 rails_env     = ENV['RAILS_ENV'] || 'development'
 resque_config = YAML.load_file(rails_root + '/config/resque.yml')
