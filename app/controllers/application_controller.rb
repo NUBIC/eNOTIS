587cbfc6d122a6061fe8df6e99773e84d0dee55e
Mark Yoon <yoon@northwestern.edu>
2010-06-29 16:05:43 -0500

    version bump

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -24,7 +24,7 @@ class ApplicationController < ActionController::Base
   # local_addresses.clear
 
   # Application version
-  APP_VERSION = "1.4.0"
+  APP_VERSION = "1.5.0"
   
   # Possible application statuses. See http://en.wikipedia.org/wiki/Bob_Dylan
   SYSTEM_STATUSES = { :up => "Don't think twice, it's all right",
