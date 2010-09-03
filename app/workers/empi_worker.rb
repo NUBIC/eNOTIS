69599bdb41e8b3d9b44931fec27eb7a1b1c2a667
Sam Granieri <sam-granieri-jr@northwestern.edu>
2010-09-03 11:37:12 -0500

    resque should handle exceptions

--- app/workers/empi_worker.rb
+++ app/workers/empi_worker.rb
@@ -2,38 +2,38 @@ require 'webservices'
 class EmpiWorker
   @queue=:empi
   def self.perform(involvement_id)
-    begin
-      # Find the involvement and the subject
-      Empi.connect EMPI_SERVICE[:uri], EMPI_SERVICE[:credentials]
-      involvement = Involvement.find(involvement_id)
-      study = involvement.study
-      if study.read_only?
-        LOG.info("[EMPI] #{Time.now} Not uploading read only study #{study.irb_number}")
-      else
-        subject = involvement.subject
-        LOG.info("[EMPI] #{Time.now} I'd be uploading subject #{subject.id} on study #{study.irb_number}")
-        # Upload the subject and involvement info to the EMPI
-        params = {
-          :source                       => "eNOTIS", 
-          :unique_id                    => subject.id.to_s,
-          :first_name                   => subject.first_name,
-          :middle_name                  => subject.middle_name, 
-          :last_name                    => subject.last_name,
-          :primary_street_address_1     => subject.address_line1,
-          :primary_street_address_2     => subject.address_line2,
-          :primary_city                 => subject.city,
-          :primary_state                => subject.state,
-          :primary_zip_code             => subject.zip.to_s,
-          :gender                       => involvement.gender, 
-          :date_of_birth                => subject.birth_date.to_s,
-          :record_creation_date         => subject.created_at.to_s
+    # begin
+    # Find the involvement and the subject
+    Empi.connect EMPI_SERVICE[:uri], EMPI_SERVICE[:credentials]
+    involvement = Involvement.find(involvement_id)
+    study = involvement.study
+    if study.read_only?
+      LOG.info("[EMPI] #{Time.now} Not uploading read only study #{study.irb_number}")
+    else
+      subject = involvement.subject
+      LOG.info("[EMPI] #{Time.now} I'd be uploading subject #{subject.id} on study #{study.irb_number}")
+      # Upload the subject and involvement info to the EMPI
+      params = {
+        :source                       => "eNOTIS", 
+        :unique_id                    => subject.id.to_s,
+        :first_name                   => subject.first_name,
+        :middle_name                  => subject.middle_name, 
+        :last_name                    => subject.last_name,
+        :primary_street_address_1     => subject.address_line1,
+        :primary_street_address_2     => subject.address_line2,
+        :primary_city                 => subject.city,
+        :primary_state                => subject.state,
+        :primary_zip_code             => subject.zip.to_s,
+        :gender                       => involvement.gender, 
+        :date_of_birth                => subject.birth_date.to_s,
+        :record_creation_date         => subject.created_at.to_s
         }.merge(calculate_mrn(subject))
-        Empi.put(params)
-        subject.update_attribute(:empi_updated_date,Time.now)
-      end
-    rescue Exception => e
-      LOG.info("[EMPI] #{Time.now} something went wrong when uploading involvement #{involvement.id}")
+      Empi.put(params)
+      subject.update_attribute(:empi_updated_date,Time.now)
     end
+    # rescue Exception => e
+    #   LOG.info("[EMPI] #{Time.now} something went wrong when uploading involvement #{involvement.id}")
+    # end
   end
 
   def self.calculate_mrn(subject)
