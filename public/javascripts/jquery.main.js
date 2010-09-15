e5db76780c58371d3f31dd0bd2f17fa129d89329
Mark Yoon <yoon@northwestern.edu>
2010-09-15 17:04:55 -0500

    more javascript cleanup, better javascript comments, cache public javascripts.

--- public/javascripts/jquery.main.js
+++ public/javascripts/jquery.main.js
@@ -1,4 +1,118 @@
-// utility functions
+$(document).ready(function() {
+  
+  // flash messages
+  $("#flash .close").click(function(){$("#flash").fadeOut(300); return false;});
+
+  // index (my studies) datatable
+  $('#my_studies .display').dataTable( {
+   "oLanguage": { "sProcessing": '/images/spinner.gif' },
+   "bProcessing": true,
+   "bServerSide": true,
+   "bRetrieve": true,
+   "sPaginationType": "full_numbers",
+   "sAjaxSource": "studies.json"
+  });
+  
+  // show study: introduction (for studies without involvements) overlay
+  $("a[rel=#intro]").overlay({
+    expose: { color: '#fff', loadSpeed: 200, opacity: 0.5 },
+  });
+  
+  // show study: datatable
+  $("#accrual .display").dataTable({
+    "fnDrawCallback": activateRows, 
+    "iDisplayLength": 30, 
+    "sPaginationType": "full_numbers", 
+    "oLanguage": {"sZeroRecords": "<p><strong>No subjects yet - click 'Add' or 'Import' to get started. Or watch our <a rel='#intro'>4 minute introduction to eNOTIS</a>.</strong></p>"},
+    "aoColumns": [null,null,null,null,null,null,null,{ "sType": "html" },{ "sType": "html" }]
+  });    
+
+  // show study: datatable paging - redraw dashes for empty cells, activate other studies and view/edit overlays
+  function activateRows(){
+    // dashes
+    $("#accrual .display td:empty, #import .display td:empty").html("--");
+    
+    // other studies overlay
+    $("a[rel=#other_studies]").overlay({
+      onBeforeLoad: function(){ $("#other_studies .wrap").load(this.getTrigger().attr("href"), "format=js", activateAccept) },
+      expose: { color: '#fff', loadSpeed: 200, opacity: 0.5 },
+      onClose: function(){$("#other_studies .wrap").html("");}
+    });
+    
+    // involvement overlay
+    $("a[rel=#involvement]").overlay({
+      onBeforeLoad: function(){ $("#involvement .wrap").load(this.getTrigger().attr("href"), "format=js"); },
+      expose: { color: '#fff', loadSpeed: 200, opacity: 0.5 },
+      onLoad: function(){
+        // dateinput
+        $("#involvement input.occurred_on").dateinput({format: 'yyyy-mm-dd', selectors: true, yearRange: [-20, 1]});
+        $("#involvement input.dob").dateinput({format: 'yyyy-mm-dd', selectors: true, yearRange: [-120, 1],
+          change: function(e, date){ e.target.getInput().val(e.target.getValue('yyyy-mm-dd')).data("date", date); return false; },
+          onShow: function(e){
+            $("#calmonth").unbind("change").change(function() { e.target.hide().setValue($("#calyear").val(), $(this).val(), e.target.getValue('d')).show(); });
+            $("#calyear").unbind("change").change(function() { e.target.hide().setValue($(this).val(), $("#calmonth").val(), e.target.getValue('d')).show(); });
+          }
+        });
+      }
+    });
+    
+    // 'delete' links
+    $('#accrual a.delete').deleteWithAjax();
+  }
+  
+  // show study: other studies overlay - bind an event onto the accept link that loads the other studies view into the same overlay
+  function activateAccept(){
+    $("#other_studies .wrap a.accept").click(function(e){
+      $("#other_studies .wrap").load($(e.target).attr("href"), "format=js");
+      e.preventDefault();
+    });
+  }
+  
+  // show study: import overlay
+  $("#actions a[rel=#import]").overlay({
+    onBeforeLoad: function(){ $("#import .wrap").load(this.getTrigger().attr("href"), "format=js"); },
+    expose: { color: '#fff', loadSpeed: 200, opacity: 0.5 }
+  });
+
+  // show study: export overlay
+  $("#actions a[rel=#export]").overlay({
+    onBeforeLoad: function(){ $("#export .wrap").load(this.getTrigger().attr("href"), "format=js"); },
+    expose: { color: '#fff', loadSpeed: 200, opacity: 0.5 }
+  });
+  
+  // show study: report overlay
+  $("a[rel=#report]").overlay({
+    expose: { color: '#fff', loadSpeed: 200, opacity: 0.5 },
+  });
+
+  // show study: study details and charts slider
+  $("#study a[rel=#study_information]").click(function(){
+    $('#study_information').slideToggle();
+    return false;
+  });
+
+  // search
+  $('#study_results .display').dataTable( {
+    "oLanguage": { "sProcessing": '/images/spinner.gif' },
+    "bProcessing": true,
+    "bServerSide": true,
+    "bRetrieve": true,
+    "sPaginationType": "full_numbers",
+    "sAjaxSource": "search.json?" + $.param({query: $.getUrlVar('query')})
+   });
+
+  // search: study information tooltips
+  $('#results a[rel=#study_information]').live('mouseover', function(event) {
+   jQuery('#results a[rel=#study_information]').tooltip({position: 'center right', offset: [-1*jQuery('#results').offset().top, -1*jQuery('#results').offset().left]});
+  });
+
+  // search page
+  // jQuery('#results a[rel=#study_information]').tooltip({position: 'center right', offset: [-1*jQuery('#results').offset().top, -1*jQuery('#results').offset().left]});
+  //  $("#subject_results .subject_display").dataTable({"aoColumns": [{ "sType": "html" }]});
+
+});
+
+// ajax for delete
 jQuery.ajaxSetup({ 'beforeSend': function(xhr) {xhr.setRequestHeader("Accept", "text/javascript")} });
 
 function _ajax_request(url, data, callback, type, method) {
@@ -43,6 +157,7 @@ jQuery.fn.deleteWithAjax = function() {
   return this;
 };
 
+// getUrlVar used in search
 $.extend({
   getUrlVars: function(){
     var vars   = [], hash;
@@ -58,120 +173,3 @@ $.extend({
     return $.getUrlVars()[name];
   }
 });
-
-
-$(document).ready(function() {
-  // Study Information Tooltips
-  $('#results a[rel=#study_information]').live('mouseover', function(event) {
-   jQuery('#results a[rel=#study_information]').tooltip({position: 'center right', offset: [-1*jQuery('#results').offset().top, -1*jQuery('#results').offset().left]});
-  });
-  
- // flash messages
-  $("#flash .close").click(function(){$("#flash").fadeOut(300); return false;});
- 
- // studies index
-  $("#my_studies a[rel=#study_information]").overlay({ 
-    onBeforeLoad: function() { var wrap = $("#study_information .contentWrap"); wrap.html(this.getTrigger().next('.study_information').html()); },
-    expose: { color: '#fff', loadSpeed: 200, opacity: 0.5 }
-  });
- 
- // My Studies
- $('#my_studies .display').dataTable( {
-   "oLanguage": {
-     "sProcessing": '/images/spinner.gif'
-   },
-   "bProcessing": true,
-   "bServerSide": true,
-   "bRetrieve": true,
-   "sPaginationType": "full_numbers",
-   "sAjaxSource": "studies.json"
- });
-
- // Searching Studies
- $('#study_results .display').dataTable( {
-   "oLanguage": {
-     "sProcessing": '/images/spinner.gif'
-   },
-   "bProcessing": true,
-   "bServerSide": true,
-   "bRetrieve": true,
-   "sPaginationType": "full_numbers",
-   "sAjaxSource": "search.json?" + $.param({query: $.getUrlVar('query')})
-  });
-
- // search page
- // jQuery('#results a[rel=#study_information]').tooltip({position: 'center right', offset: [-1*jQuery('#results').offset().top, -1*jQuery('#results').offset().left]});
- //  $("#subject_results .subject_display").dataTable({"aoColumns": [{ "sType": "html" }]});
- 
- // studies show
-    // dataTable
-    $("#accrual .display").dataTable({"fnDrawCallback": activateRows, "iDisplayLength": 30, "sPaginationType": "full_numbers", "oLanguage": {"sZeroRecords": "<p><strong>No subjects yet - click 'Add' or 'Import' to get started. Or watch our <a rel='#intro'>4 minute introduction to eNOTIS</a>.</strong></p>"},"aoColumns": [null,null,null,null,null,null,null,{ "sType": "html" },{ "sType": "html" }]});    
-    
-    // 'delete' links
-    $('#accrual a.delete').deleteWithAjax();
-    
-    // redraw dashes for empty cells, activate other studies and view/edit overlays
-    function activateRows(){
-      $("#accrual .display td:empty, #import .display td:empty").html("--");
-      
-      // other studies overlay
-      $("a[rel=#other_studies]").overlay({
-        onBeforeLoad: function(){ $("#other_studies .wrap").load(this.getTrigger().attr("href"), "format=js", activateAccept) },
-        expose: { color: '#fff', loadSpeed: 200, opacity: 0.5 },
-        onClose: function(){$("#other_studies .wrap").html("");}
-      });
-      
-      // involvement overlay
-      $("a[rel=#involvement]").overlay({
-        onBeforeLoad: function(){ $("#involvement .wrap").load(this.getTrigger().attr("href"), "format=js"); },
-        expose: { color: '#fff', loadSpeed: 200, opacity: 0.5 },
-        onLoad: function(){
-          $("#involvement input.occurred_on").click(function(){console.log($(this).offset());}).dateinput({format: 'yyyy-mm-dd', selectors: true, yearRange: [-20, 1]});
-          $("#involvement input.dob").click(function(){console.log($(this).offset()); console.log($(this).outerHeight({margins: true}));}).dateinput({format: 'yyyy-mm-dd', selectors: true, yearRange: [-120, 1],
-            change: function(e, date){ e.target.getInput().val(e.target.getValue('yyyy-mm-dd')).data("date", date); return false; },
-            onShow: function(e){
-              $("#calmonth").unbind("change").change(function() { e.target.hide().setValue($("#calyear").val(), $(this).val(), e.target.getValue('d')).show(); });
-              $("#calyear").unbind("change").change(function() { e.target.hide().setValue($(this).val(), $("#calmonth").val(), e.target.getValue('d')).show(); });
-            }
-          });
-        }
-      });
-    }
-    
-    // introduction overlay
-    $("a[rel=#intro]").overlay({
-      expose: { color: '#fff', loadSpeed: 200, opacity: 0.5 },
-    });
-    
-    // import overlay
-    $("#actions a[rel=#import]").overlay({
-      onBeforeLoad: function(){ $("#import .wrap").load(this.getTrigger().attr("href"), "format=js"); },
-      expose: { color: '#fff', loadSpeed: 200, opacity: 0.5 }
-    });
-
-    // export overlay
-    $("#actions a[rel=#export]").overlay({
-      onBeforeLoad: function(){ $("#export .wrap").load(this.getTrigger().attr("href"), "format=js"); },
-      expose: { color: '#fff', loadSpeed: 200, opacity: 0.5 }
-    });
-    
-    // report overlay
-    $("a[rel=#report]").overlay({
-      expose: { color: '#fff', loadSpeed: 200, opacity: 0.5 },
-    });
-        
-    // bind an event onto the accept link that loads the other studies view into the same overlay
-    function activateAccept(){
-      $("#other_studies .wrap a.accept").click(function(e){
-        $("#other_studies .wrap").load($(e.target).attr("href"), "format=js");
-        e.preventDefault();
-      });
-    }
-        
-    // study information
-    $("#study a[rel=#study_information]").click(function(){
-      $('#study_information').slideToggle();
-      return false;
-    });
-});
-
--- public/javascripts/jquery.public.js
+++ public/javascripts/jquery.public.js
@@ -1,13 +1,24 @@
 $(document).ready(function(){
-  // one less click for login
-  $("#netid").focus();
-  // overlay
-  $("a[rel=#help]").overlay({onBeforeLoad: function(){var wrap = this.getContent().find(".contentWrap"); wrap.load(this.getTrigger().attr("href"), "format=js");}, expose: {color: '#fff', loadSpeed: 200, opacity: 0.5}});
-  $("a[rel=#database-language]").overlay({expose: {color: '#fff', loadSpeed: 200, opacity: 0.5}});
-  // scrollable
-  $("div.panes").scrollable({size: 1}).navigator({navi: "ul.tabs", naviItem: "a", activeClass: "current"});
   // flash (messages)
   $("#flash .close").click(function(){$("#flash").fadeOut(300);});
+
+  // one less click for login
+  $("#netid").focus();
+
+  // login help overlay
+  $("a[rel=#help]").overlay({
+    onBeforeLoad: function(){ $("#help .contentWrap").load(this.getTrigger().attr("href"), "format=js"); },
+    expose: {color: '#fff', loadSpeed: 200, opacity: 0.5}
+  });
+  
+  // scrollable
+  $("div.panes").scrollable({size: 1}).navigator({navi: "ul.tabs", naviItem: "a", activeClass: "current"});
+  
+  // db language overlay
+  $("a[rel=#database-language]").overlay({expose: {color: '#fff', loadSpeed: 200, opacity: 0.5}});
+      
   // video
-  flowplayer("player", "/media/flowplayer-3.2.2.swf", {clip: {autoPlay: false}, canvas: {backgroundColor: '#dddddd', backgroundGradient: 'medium'}});
+  if($('#player').length){
+    flowplayer("player", "/media/flowplayer-3.2.4.swf", {clip: {autoPlay: false}, canvas: {backgroundColor: '#dddddd', backgroundGradient: 'medium'}});
+  }
 });
