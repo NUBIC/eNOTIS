9b43b42f206c6ad34c02cac71d4fd5570445d752
Sam Granieri <sam-granieri-jr@northwestern.edu>
2010-09-16 13:22:24 -0500

    delegate funding source to study table. closes #3984

--- app/models/study_table.rb
+++ app/models/study_table.rb
@@ -3,6 +3,6 @@ class StudyTable < ActiveRecord::Base
   belongs_to :study, :foreign_key => "id"
   named_scope :paged, lambda {|start, limit| { :offset => start ,:limit => limit}}
   named_scope :order, lambda {|order| { :order => order}}
-  delegate :may_accrue?, :has_coordinator? , :approved_date, :expiration_date, :closed_or_completed_date, :research_type, :to => :study
+  delegate :may_accrue?, :has_coordinator? , :approved_date, :expiration_date, :closed_or_completed_date, :research_type, :funding_sources, :to => :study
   
 end
