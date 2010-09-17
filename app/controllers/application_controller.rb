a09142577fb0fd46345bd92cac4ce7b374782f6a
Sam Granieri <sam-granieri-jr@northwestern.edu>
2010-09-17 15:29:28 -0500

    Releasing v1.7.1. One quick UI fix for Laura on the hub page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -23,7 +23,7 @@ class ApplicationController < ActionController::Base
   # local_addresses.clear
 
   # Application version
-  APP_VERSION = "1.7.1.pre"
+  APP_VERSION = "1.7.1"
   
   # Possible application statuses. See http://en.wikipedia.org/wiki/Bob_Dylan
   SYSTEM_STATUSES = { :up => "Don't think twice, it's all right",
