104cd696f310d99d366ced55c3acd58318271b6d
Mark Yoon <yoon@northwestern.edu>
2010-12-22 17:14:08 -0600

    form mode for hudson cucumber tests

--- config/environments/hudson.rb
+++ config/environments/hudson.rb
@@ -36,6 +36,7 @@ config.after_initialize do
   Bcsec.configure do
     static = Bcsec::Authorities::Static.from_file(File.expand_path("../../static_auth.yml", __FILE__))
     enotis = Bcsec::Authorities::Enotis.new
+    ui_mode :form
     authorities static, :pers, enotis
     cas_parameters :base_url => "http://fake.cas.example.org"
   end
