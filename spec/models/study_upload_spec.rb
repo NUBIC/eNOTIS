a0291ab89ea327bbcb2ae4204b924eb6ede30d0c
Mark Yoon <yoon@northwestern.edu>
2010-12-23 10:38:58 -0600

    fixed failing feature, due to upload spec with nested describes with before(:all)

--- spec/models/study_upload_spec.rb
+++ spec/models/study_upload_spec.rb
@@ -6,7 +6,6 @@ def up(name)
 end
 
 describe StudyUpload do
-  
   it "should create a new instance given valid attributes" do
     Factory(:study_upload).should be_valid
   end
@@ -50,63 +49,64 @@ describe StudyUpload do
     @up.legit?.should be_false
     @up.summary.should =~ /and fix/
   end
+end 
+describe "should be successful with a good csv and store the data it is supposed to!" do
+
+  before(:each) do
+    StudyUpload.count.should == 0
+    @up = Factory(:study_upload, :upload => up('single_subject_good'))
+    @study = @up.study
+  end
+
+  it "should actually upload the file" do
+    @up.upload_exists?.should be_true
+  end
+
+  it "parses the upload" do
+    @up.parse_upload.should be_true
+  end
+
+  it "creates the subjects" do
+    @up.create_subjects.should be_true
+    @up.summary.should =~ /1 subjects/
+    @study.involvements.should have(1).subjects
+  end
+end
+describe "checking the data against the upload" do 
+  # Header, for example:
+  # <FasterCSV::Row "Case Number":"14212" "NMFF MRN":nil "NMH MRN":nil "RIC MRN":nil "First Name":"Meghan" "Last Name":"O'Keefe"
+  # "Birth Date":"1/3/1956" "Gender":"Female" "Race":"Unknown or Not Reported" "Ethnicity":"Unknown or Not Reported" 
+  # "Consented On":"1/2/2010" "Consented Note":nil "Withdrawn On":"5/4/2010" "Withdrawn Note":nil "Completed On":nil 
+  # "Completed Note":nil>
   
-  describe "should be successful with a good csv and store the data it is supposed to!" do
+  before(:each) do
+    @up = Factory(:study_upload, :upload => up('single_subject_good'))
+    @study = @up.study
+    
+    @up.create_subjects.should be_true
+    @up.summary.should =~ /1 subjects/
+    @inv = @study.involvements.first
+    @csv = FasterCSV.parse(up('single_subject_good'), :headers => true)
+    @row = @csv[0]
+  end
 
-    before(:all) do
-      @up = Factory(:study_upload, :upload => up('single_subject_good'))
-      @study = @up.study
-    end
+  it "has the correct subject data" do
+    @row["Case Number"].should == @inv.case_number  
+    @row["First Name"].should == @inv.subject.first_name
+    @row["Last Name"].should  == @inv.subject.last_name
+    @row["Birth Date"].should == @inv.subject.birth_date.strftime("%m/%d/%Y")
+  end
 
-    it "should actually upload the file" do
-      @up.upload_exists?.should be_true
-    end
+  it "has the correct race, gender, etc" do
+    @row["Gender"].should == @inv.gender
+    @row["Race"].should == @inv.race.to_s
+    @row["Ethnicity"].should == @inv.ethnicity
+  end
 
-    it "parses the upload" do
-      @up.parse_upload.should be_true
-    end
-
-    it "creates the subjects" do
-      @up.create_subjects.should be_true
-      @up.summary.should =~ /1 subjects/
-      @study.involvements.should have(1).subjects
-    end
-
-    describe "checking the data against the upload" do 
-      # Header, for example:
-      # <FasterCSV::Row "Case Number":"14212" "NMFF MRN":nil "NMH MRN":nil "RIC MRN":nil "First Name":"Meghan" "Last Name":"O'Keefe"
-      # "Birth Date":"1/3/1956" "Gender":"Female" "Race":"Unknown or Not Reported" "Ethnicity":"Unknown or Not Reported" 
-      # "Consented On":"1/2/2010" "Consented Note":nil "Withdrawn On":"5/4/2010" "Withdrawn Note":nil "Completed On":nil 
-      # "Completed Note":nil>
-      #
-
-      before(:all) do
-        @up.create_subjects.should be_true
-        @up.summary.should =~ /1 subjects/
-        @inv = @study.involvements.first
-        @csv = FasterCSV.parse(up('single_subject_good'), :headers => true)
-        @row = @csv[0]
-      end
-
-      it "has the correct subject data" do
-        @row["Case Number"].should == @inv.case_number  
-        @row["First Name"].should == @inv.subject.first_name
-        @row["Last Name"].should  == @inv.subject.last_name
-        @row["Birth Date"].should == @inv.subject.birth_date.strftime("%m/%d/%Y")
-      end
-
-      it "has the correct race, gender, etc" do
-        @row["Gender"].should == @inv.gender
-        @row["Race"].should == @inv.race.to_s
-        @row["Ethnicity"].should == @inv.ethnicity
-      end
-
-      it "has the correct events" do
-        c_event = @inv.event_detect("Consented")
-        c_event.occurred_on.strftime("%m/%d/%Y").should == @row["Consented On"]
-        w_event = @inv.event_detect("Withdrawn")
-        w_event.occurred_on.strftime("%m/%d/%Y").should == @row["Withdrawn On"]
-      end
-    end
+  it "has the correct events" do
+    c_event = @inv.event_detect("Consented")
+    c_event.occurred_on.strftime("%m/%d/%Y").should == @row["Consented On"]
+    w_event = @inv.event_detect("Withdrawn")
+    w_event.occurred_on.strftime("%m/%d/%Y").should == @row["Withdrawn On"]
   end
 end
