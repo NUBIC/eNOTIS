ab4b2530981eae2a9b101fedc1ac726a47eba8e6
Brian Chamberlain <b-chamberlain@northwestern.edu>
2011-01-07 14:23:42 -0600

    added new parameter, and tests for roles webservice

--- app/controllers/roles_controller.rb
+++ app/controllers/roles_controller.rb
@@ -3,7 +3,13 @@ class RolesController < ApplicationController
     @user = User.find_by_netid(params[:id], :include => {:roles => :study})
     respond_to do |format|
       format.json do
-        render :json => @user.roles.map{|r| {:consent_role => r.consent_role, :project_role => r.project_role, :irb_number => r.study.irb_number, :study_name => r.study.name} }
+        render :json => @user.roles.map{|r| {
+          :consent_role => r.consent_role, 
+          :project_role => r.project_role,
+          :irb_number => r.study.irb_number,
+          :study_name => r.study.name,
+          :study_approved_date => r.study.approved_date.strftime('%Y-%m-%d')
+        } }
       end
       format.html
     end
---
+++ spec/controllers/roles_controller_spec.rb
@@ -0,0 +1,19 @@
+require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
+
+describe RolesController do
+
+  #Delete this example and add some real ones
+  it "should use RolesController" do
+    controller.should be_an_instance_of(RolesController)
+  end
+
+  # Registar and clearcats need this to exist 
+  it "should respond with role data for GET requests including netids" do
+    user = Factory(:user, :netid => 'baa321')
+    role = Factory(:role, :user => user, :study => Factory(:study, :approved_date => 1.year.ago))
+    User.should_receive(:find_by_netid).and_return(user)
+    get :show, {:id => "baa321"}
+    response.should be_success
+  end
+
+end
