3033cb841c2c5daf0d8d2d1ebf3b060fefbc6f51
Brian Chamberlain <b-chamberlain@northwestern.edu>
2011-01-13 20:03:06 -0600

    specs and features passing now

--- app/controllers/roles_controller.rb
+++ app/controllers/roles_controller.rb
@@ -1,20 +1,25 @@
 class RolesController < ApplicationController
   def show
+    @user = Bcsec.authority.find_user(params[:id])
     @roles = Role.find_all_by_netid(params[:id], :include => {:study => [:involvement_events, :event_types]})
-    if !@roles.empty?
-      respond_to do |format|
-        format.json do
-          render :json => @roles.map{|r| {
+    respond_to do |format|
+      format.json do
+        unless @roles.empty?
+        render :json => @roles.map{|r| {
           :consent_role => r.consent_role, 
           :project_role => r.project_role,
           :irb_number => r.study.irb_number,
           :study_name => r.study.name,
           :study_approved_date => (r.study.approved_date.nil?) ? r.study.approved_date : r.study.approved_date.strftime('%Y-%m-%d')
-          } }
+        } }
+        else
+          render :text => "Not found", :status => 404
         end
       end
-    else
-      render :text => "Not found", :status => 404
+      format.html do
+        render
+      end
+
     end
- end
+  end
 end
