3c7bd594e178340d8defceac3adbcf594428a784
Brian Chamberlain <b-chamberlain@northwestern.edu>
2011-01-20 08:40:39 -0600

    added services survey controller

---
+++ spec/controllers/services_controller_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+
+describe ServicesController do
+
+  #Delete this example and add some real ones
+  it "should use ServicesController" do
+    controller.should be_an_instance_of(ServicesController)
+  end
+
+end
