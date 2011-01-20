3e94663158b501bcaecc7a86ae3d0bd87323ddff
Mark Yoon <yoon@northwestern.edu>
2011-01-20 15:58:46 -0600

    added services, reports, and procedures to study

---
+++ db/migrate/20110120214455_create_service_reports.rb
@@ -0,0 +1,24 @@
+class CreateServiceReports < ActiveRecord::Migration
+  def self.up
+  	create_table :service_reports do |t|
+  	  t.integer :study_id
+      t.boolean :uses_medical_services
+      t.integer :current_enrollment
+      t.integer :expected_enrollment
+      t.integer :expected_services
+      t.boolean :requires_outpatient
+      t.boolean :requires_inpatient
+      t.integer :inpatient_days_research
+      t.integer :inpatient_days_standard
+      t.boolean :involves_pharmacy
+      t.boolean :involves_labs
+      t.boolean :involves_imaging
+      t.string  :contact_name
+      t.string  :contact_email
+      t.string  :contact_phone
+    end
+  end
+  def self.down
+    drop_table :service_reports
+  end
+end
