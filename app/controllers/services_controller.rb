95aade30f2ad670c5e0fb2295f935039a4ee499e
Brian Chamberlain <b-chamberlain@northwestern.edu>
2011-01-22 17:44:16 -0600

    basic forms working, doesn't look very good

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -5,12 +5,17 @@ class ServicesController < ApplicationController
   
   def index
     @studies = current_user ? current_user.studies : []
+    @service_studies = @studies.select{|s| s.uses_medical_services == true }
   end
 
   def edit
     @study = Study.find_by_irb_number(params[:id])
     return redirect_to services_path unless @study
-    @medical_service = @study.medical_service || @study.medical_service.build
+    @medical_service = @study.medical_service || @study.build_medical_service
+  end
+
+  def update
+
   end
 
 end
