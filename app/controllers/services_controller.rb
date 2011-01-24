0b43780f382a023c5d092d37ffeb37e150779c27
Brian Chamberlain <b-chamberlain@northwestern.edu>
2011-01-24 12:04:38 -0600

    updated styles, and layout

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -4,11 +4,13 @@ class ServicesController < ApplicationController
   permit :user
   
   def index
+    @title = "Medical Services"
     @studies = current_user ? current_user.studies : []
     @service_studies = @studies.select{|s| s.uses_medical_services == true }
   end
 
   def edit
+    @title = "Medical Services Form"
     @study = Study.find_by_irb_number(params[:id])
     return redirect_to services_path unless @study
     @medical_service = @study.medical_service || @study.build_medical_service
