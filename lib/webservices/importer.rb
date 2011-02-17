0fa92bef6e2e304aedb187b78b1c09637464147f
Brian Chamberlain <b-chamberlain@northwestern.edu>
2011-02-17 15:34:27 -0600

    improved function and tests for study import sanitation

--- lib/webservices/importer.rb
+++ lib/webservices/importer.rb
@@ -16,7 +16,7 @@ module Webservices
         false
       end
     end
- 
+
     # Some method to handle the storing 
     # of the data we are importing to the study
     # Just used to store the most recent update
@@ -31,7 +31,7 @@ module Webservices
       init_import_cache
       self.import_cache.merge!(val)
     end
-  
+
     def import_results
       init_import_cache
       self.import_cache
@@ -67,12 +67,12 @@ module Webservices
 
       def import_external_study_data(study)
         study_set = {:raw =>{:errors => []}, :clean => {:errors => []}} # setting our temporary data hold
-        
+
         # Querying the sources
         study_set[:raw][:study] = query_study_source(study.irb_number)
         study_set[:raw][:roles] = query_roles_source(study.irb_number)
         #study_set[:subjects] = query_subjects_source(study.irb_number)
-        
+
         # Cleaning the results 
         study_set[:clean] = sanitize(study_set[:raw])
 
@@ -109,54 +109,78 @@ module Webservices
       end
 
       def sanitize_study(study_set)
-         study = {}
-         # pulling out the data from the hash structure where the hash 
-         # results are under the name of the query
-         study_set.each_value{|v| study.merge!(v)}
+        study = {}
+        # pulling out the data from the hash structure where the hash 
+        # results are under the name of the query
+        unless study_set[:find_basics].empty?  
+          bh = study_set[:find_basics].first
+          study[:approved_date] = Chronic.parse(bh[:approved_date])
+          study[:expired_date] = Chronic.parse(bh[:expired_date])
+          study[:expiration_date] = Chronic.parse(bh[:expiration_date])
+          study[:created_date] = Chronic.parse(bh[:created_date])
+          study[:modified_date] = Chronic.parse(bh[:modified_date])
+          study[:closed_or_completed_date] = Chronic.parse(bh[:closed_or_completed_date])
 
-         return {
-           :accrual_goal                      => study[:accrual_goal], 
-           :approved_date                     => Chronic.parse(study[:approved_date]),
-           :clinical_trial_submitter          => study[:clinical_trial_submitter], 
-           :closed_or_completed_date          => Chronic.parse(study[:closed_or_completed_date]),
-           :created_date                      => Chronic.parse(study[:created_date]),
-           :description                       => study[:description],
-           :exclusion_criteria                => study[:exclusion_criteria],
-           :expiration_date                   => Chronic.parse(study[:expiration_date]),
-           :expired_date                      => Chronic.parse(study[:expired_date]), 
-           :fda_offlabel_agent                => study[:fda_offlabel_agent],
-           :fda_unapproved_agent              => study[:fda_unapproved_agent],
-           :inclusion_criteria                => study[:inclusion_criteria],
-           :irb_status                        => study[:irb_status],
-           :is_a_clinical_investigation       => study[:is_a_clinical_investigation],
-           :modified_date                     => Chronic.parse(study[:modified_date]),
-           :name                              => study[:name],
-           :periodic_review_open              => study[:periodic_review_open],
-           :research_type                     => study[:research_type],
-           :review_type_requested             => study[:review_type_requested],
-           :subject_expected_completion_count => study[:subject_expected_completion_count],
-           :title                             => study[:title],
-           :total_subjects_at_all_ctrs        => study[:total_subjects_at_all_ctrs]
-        } 
+          study[:periodic_review_open] = bh[:periodic_review_open]
+          study[:accrual_goal] = bh[:accrual_goal]
+          study[:name] = bh[:name]
+          study[:is_a_clinical_investigation] = bh[:is_a_clinical_investigation]
+          study[:fda_offlabel_agent] = bh[:fda_offlabel_agent]
+          study[:fda_unapproved_agent] = bh[:fda_unapproved_agent]
+          study[:irb_status] = bh[:irb_status]
+          study[:subject_expected_completion_count] = bh[:subject_expected_completion_count]
+          study[:closed_or_completed_date] = Chronic.parse(bh[:closed_or_completed_date])
+          study[:clinical_trial_submitter] = bh[:clinical_trial_submitter] 
+          study[:research_type] = bh[:research_type]
+          study[:review_type_requested] = bh[:review_type_requested]
+          study[:title] = bh[:title]
+          study[:total_subjects_at_all_ctrs] = bh[:total_subjects_at_all_ctrs]
+        end
+
+        if !study_set[:find_description].empty? and study_set[:find_description].first[:description]
+          study[:description] = study_set[:find_description].first[:description]
+        end
+
+        if !study_set[:find_inc_excl].empty?
+          incl = study_set[:find_inc_excl].first
+          study[:exclusion_criteria] = incl[:exclusion_criteria]
+          study[:inclusion_criteria] = incl[:inclusion_criteria]
+        end
+
+        study_set[:find_funding_sources].each do |fs|
+          study[:funding_sources] ||= []
+          study[:funding_sources] << {:name => fs[:funding_source_name], :code => fs[:funding_source_id], :category => fs[:funding_source_category_name]}
+        end
+
+        return study
       end
 
       def sanitize_roles(roles_set)
         roles = []
-        roles_set[:find_principal_investigators].each do |pi|
-          roles << {:netid => pi[:netid], :project_role => "Principal Investigator", :consent_role => "Obtaining"}
+        if roles_set[:find_principal_investigators]
+          roles_set[:find_principal_investigators].each do |pi|
+            # note: we set the roles here, so all that's really needed is the netid
+            roles << {:netid => pi[:netid], :project_role => "Principal Investigator", :consent_role => "Obtaining"}
+          end
         end
-        roles_set[:find_co_investigators].each do |ci|
-          roles << {:netid => ci[:netid], :project_role => "Co-Investigator", :consent_role => "Obtaining"}
+        if roles_set[:find_co_investigators]
+          roles_set[:find_co_investigators].each do |ci|
+            # note: we set the roles here, so all that's really needed is the netid
+            roles << {:netid => ci[:netid], :project_role => "Co-Investigator", :consent_role => "Obtaining"}
+          end
         end
-        roles_set[:find_authorized_personnel].each do |ap|
-          roles << {:netid => ap[:netid], :project_role => ap[:project_role], :consent_role => ap[:consent_role]}
+        if roles_set[:find_authorized_personnel]
+          roles_set[:find_authorized_personnel].each do |ap|
+            # We need more than just netid here
+            roles << {:netid => ap[:netid], :project_role => ap[:project_role], :consent_role => ap[:consent_role]}
+          end
         end
         roles.reject! do |r|
           r[:netid].blank? or r[:project_role].blank? or r[:consent_role].blank?
         end
         roles
       end
-     
+
       # These are the setup methods for a target
       # data model. They contain the list of queries to run
       # the target data source, and the only require the 
@@ -182,8 +206,8 @@ module Webservices
 
       def query_subjects_source(irb_number)
         query_list = [
-         :subject_import_from_NOTIS,
-         :subject_import_from_ANES
+          :subject_import_from_NOTIS,
+          :subject_import_from_ANES
         ]
         do_import_queries(Edw, irb_number, query_list)
       end
@@ -213,10 +237,10 @@ module Webservices
               :error => err.to_s, 
               :at => Time.now,
               :request => {
-                :source => source_system,
-                :irb_number => irb_number,
-                :query => query
-              }
+              :source => source_system,
+              :irb_number => irb_number,
+              :query => query
+            }
             }
           end
         end
--- spec/lib/importer_spec.rb
+++ spec/lib/importer_spec.rb
@@ -63,12 +63,95 @@ describe Webservices::Importer do
       end
 
       it "should sanitize roles to remove bad data" do
-        bad = [{:netid => "abc123"}]
+        bad = {:find_authorized_personnel => [{:netid => "abc123"}]}
         Webservices::Importer.sanitize_roles(bad).should be_empty
-        good = [{:netid => "abc123", :project_role => "PI", :consent_role => "Obtaining"}]
+        good = {:find_authorized_personnel => [{:netid => "abc123", :project_role => "PI", :consent_role => "Obtaining"}]}
         Webservices::Importer.sanitize_roles(good).should_not be_empty
       end
-  
+
+      it "should sanitize the data from the study query set" do
+        # taken off the wire
+        example = {
+          :find_inc_excl=>[
+          {
+          :exclusion_criteria=>"Old people", 
+          :inclusion_criteria=>"Young people", 
+          :irb_number=>"STU00019833", 
+          :population_protocol_page=>"N/A",
+          :should_not_include => "fake attr"
+          }], 
+            :find_funding_sources=>[
+              {
+                :funding_source_category_name=>"Institution", 
+                :irb_number=>"STU00019833", 
+                :funding_source_name=>"Northwestern", 
+                :funding_source_id=>"NU"
+          }], 
+            :find_basics=>[
+              {
+                :completed_date=>"", 
+                :irb_status=>"Approved", 
+                :approved_date=>"12/4/2009", 
+                :expired_date=>"12/31/2500 2:00:00 AM", 
+                :periodic_review_open=>"false", 
+                :expiration_date=>"12/3/2011", 
+                :total_subjects_at_all_ctrs=>"", 
+                :clinical_trial_submitter=>"", 
+                :subject_expected_completion_count=>"", 
+                :review_type_requested=>"Full IRB Review", 
+                :irb_number=>"STU00019833", 
+                :title=>"NUCATS Research Subject Registration System (eNOTIS)", 
+                :closed_or_completed_date=>"", 
+                :fda_unapproved_agent=>"", 
+                :created_date=>"9/24/2009 1:30:37 PM", 
+                :accrual_goal=>"", 
+                :fda_offlabel_agent=>"", 
+                :modified_date=>"11/15/2010 10:50:41 AM", 
+                :name=>"N1003-eNOTIS", 
+                :research_type=>"Bio-medical", 
+                :is_a_clinical_investigation=>"false"
+          }], 
+            :find_description=>[
+              {
+                :irb_number=>"STU00019833", 
+                :description=>"Description of study"
+          }]
+        }
+
+        study_hash = Webservices::Importer.sanitize_study(example)
+        study_hash[:completed_date].should be_nil
+        study_hash[:irb_status].should == "Approved"
+        study_hash[:approved_date].should_not be_nil
+        # checking the funding sources are correct
+        fs = study_hash[:funding_sources].first
+        fs[:name].should == "Northwestern"
+        fs[:code].should == "NU"
+        fs[:category].should == "Institution"
+
+        # checkin incl/excl criteria
+        study_hash[:exclusion_criteria].should == "Old people"
+        study_hash[:inclusion_criteria].should == "Young people"
+        # checking the descr
+        study_hash[:description].should == "Description of study"
+
+        # the fake hash key is not included
+        study_hash[:should_not_include].should be_nil
+      end
+ 
+      it "should not crash if some queries return empty arrays" do
+        example = {
+          :find_inc_excl=>[], 
+          :find_funding_sources=>[], 
+          :find_basics=>[], 
+          :find_description=>[]
+        }
+
+        study_hash = Webservices::Importer.sanitize_study(example)
+        study_hash.each_value do |val|
+          val.should be_nil
+        end
+      end
+
       describe "error handling" do
 
         it "should write the error to the data hash if a query fails" do
