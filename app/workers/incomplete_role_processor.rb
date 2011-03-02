a3dfa0238d4bccbfd580b29cb6e535522b3fa06e
Brian Chamberlain <b-chamberlain@northwestern.edu>
2011-03-02 09:24:47 -0600

    removed a bunch of workers which we no longer use

--- app/workers/incomplete_role_processor.rb
+++
@@ -1,5 +0,0 @@
-class IncompleteRoleProcessor
-  @queue = :redis_incomplete_role
-  def self.perform(irb_number, netid, project_role, consent_role)
-  end
-end
--- app/workers/missing_netid_processor.rb
+++
@@ -1,5 +0,0 @@
-class MissingNetidProcessor
-  @queue = :redis_missing_netid
-  def self.perform(irb_number, project_role, consent_role)
-  end
-end
