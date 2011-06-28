334ec975e137f5f9b90c8398bc6929986f63b83a
Rhett Sutphin <rhett@detailedbalance.net>
2011-06-28 16:03:04 -0500

    Whitespace.

--- lib/users_to_pers.rb
+++ lib/users_to_pers.rb
@@ -1,6 +1,6 @@
 module UsersToPers
   PORTAL = "eNOTIS" # should match Bcsec.configuration.portal in config/initializers/bcsec.rb
-  
+
   # Creates the eNOTIS portal as well as Admin and User groups in cc_pers
   def self.setup
     unless enotis = Pers::Portal.find_by_name(PORTAL)
@@ -14,8 +14,20 @@ module UsersToPers
   end
   # Creates eNOTIS developers and admins in cc_pers
   def self.create_admins
-    %w(pny668 Peter Nyberg blc615 Brian Chamberlain daw286 David Were lmw351 Laura Wimbiscus\ Yoon myo628 Mark Wimbiscus\ Yoon wakibbe Warren Kibbe).each_slice(3) do |netid, first_name, last_name|
-      Pers::Person.create(:username => netid, :first_name => first_name, :last_name => last_name, :entered_by => "enotis-application") unless Pers::Person.find_by_username(netid)
+    %w(
+      pny668 Peter Nyberg
+      blc615 Brian Chamberlain
+      daw286 David Were
+      lmw351 Laura Wimbiscus\ Yoon
+      myo628 Mark Wimbiscus\ Yoon
+      wakibbe Warren Kibbe
+    ).each_slice(3) do |netid, first_name, last_name|
+      unless Pers::Person.find_by_username(netid)
+        Pers::Person.create(
+          :username => netid, :first_name => first_name, :last_name => last_name,
+          :entered_by => "enotis-application"
+          )
+      end
       unless Pers::Login.find_by_username_and_portal(netid, PORTAL)
         login = Pers::Login.new
         login.username = netid
@@ -23,11 +35,14 @@ module UsersToPers
         login.save
       end
       puts "updated/created #{netid} as admin"
-      Pers::GroupMembership.create(:username => netid, :group_name => "Admin", :portal => PORTAL) unless Pers::GroupMembership.find_by_username_and_portal_and_group_name(netid, PORTAL, "Admin")
-      Pers::GroupMembership.create(:username => netid, :group_name => "User", :portal => PORTAL) unless Pers::GroupMembership.find_by_username_and_portal_and_group_name(netid, PORTAL, "User")
+      %w(Admin User).each do |group|
+        unless Pers::GroupMembership.find_by_username_and_portal_and_group_name(netid, PORTAL, group)
+          Pers::GroupMembership.create(:username => netid, :group_name => group, :portal => PORTAL)
+        end
+      end
     end
   end
- 
+
   def self.insert_user_into_cc_pers(netid, attrs, role = "User")
     Bcaudit::AuditInfo.current_user = Bcsec.authority.find_user('blc615')
     unless Pers::Person.find_by_username_or_id(netid)
@@ -44,7 +59,7 @@ module UsersToPers
         login.portal_name = PORTAL # can't be bulk set
         if login.save
           puts "login saved - #{netid} eNOTIS"
-        else 
+        else
           puts "login not saved - #{netid} eNOTIS (#{login.errors.full_messages})"
         end
     end
@@ -57,7 +72,7 @@ module UsersToPers
       end
     end
   end
-  
+
   # Find users in cc_pers
   # TODO: untested!
   def self.user_lookup(redis, study, netid, project_role, consent_role)
