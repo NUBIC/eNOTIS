68fd316c59eb6d0016036a3bd34ab58657297dce
John Dzak <john.dzak@gmail.com>
2011-07-20 17:20:41 -0500

    Added the full and partial eIRB import tasks to the whenever schedule. Closes #6003.

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,3 +1,11 @@
 every 1.day do
   rake 'subjects:empi_upload'
 end
+
+every :saturday, :at => '10pm' do
+  rake 'importer:full_mutha_trucking_update', :output => '/var/www/apps/enotis/shared/log/import_cron.log'
+end
+
+every :weekday, :at => '3am' do
+  rake 'importer:priority_update', :output => '/var/www/apps/enotis/shared/log/import_cron.log'
+end
