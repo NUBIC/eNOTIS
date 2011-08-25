55212ba779a61adc5f5113805e7d316e45eec169
kumbafu <mutero_d@yahoo.com>
2011-08-25 16:25:58 -0500

    fixed migration error - removed display_order

--- db/migrate/20110824133942_add_survey_groups.rb
+++ db/migrate/20110824133942_add_survey_groups.rb
@@ -6,12 +6,10 @@ class AddSurveyGroups < ActiveRecord::Migration
       t.string :progression
     end
     add_column :surveys, :survey_group_id, :integer
-    add_column :surveys, :display_order, :integer
   end
 
   def self.down
     drop_table :survey_groups
     remove_column :surveys, :survey_group_id
-    remove_column :surveys, :display_order
   end
 end
