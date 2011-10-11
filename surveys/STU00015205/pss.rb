9363458d4193ca7723f9c974c89180f4315a907f
kumbafu <mutero_d@yahoo.com>
2011-10-11 10:55:01 -0500

    fixed typo in pss response set

--- surveys/STU00015205/pss.rb
+++ surveys/STU00015205/pss.rb
@@ -8,25 +8,25 @@ survey "Perceived Stress Scale" ,:irb_number=>'STU00015205',:score_configuration
       a_1 "Never",:weight=>0
       a_2 "Almost Never",:weight=>1
       a_3 "Sometimes",:weight=>2
-      a_4 "Fair Often",:weight=>3
+      a_4 "Fairly Often",:weight=>3
       a_5 "Very Often",:weight=>4
       q_pss_2 "In the last month, how often have you felt confident about your ability to handle your personal problems?",:pick=>:one,:score_code=>"reverse"
       a_1 "Never",:weight=>4
       a_2 "Almost Never",:weight=>3
       a_3 "Sometimes",:weight=>2
-      a_4 "Fair Often",:weight=>1
+      a_4 "Fairly Often",:weight=>1
       a_5 "Very Often",:weight=>0
       q_pss_3 "In the last month, how often have you felt that things were going your way?",:pick=>:one,:score_code=>"reverse"
       a_1 "Never",:weight=>4
       a_2 "Almost Never",:weight=>3
       a_3 "Sometimes",:weight=>2
-      a_4 "Fair Often",:weight=>1
+      a_4 "Fairly Often",:weight=>1
       a_5 "Very Often",:weight=>0
       q_pss_4 "In the last month, how often have you felt difficulties were piling up so high that you could not overcome them?",:pick=>:one,:score_code=>"forward"
       a_1 "Never",:weight=>0
       a_2 "Almost Never",:weight=>1
       a_3 "Sometimes",:weight=>2
-      a_4 "Fair Often",:weight=>3
+      a_4 "Fairly Often",:weight=>3
       a_5 "Very Often",:weight=>4
     end
   end
