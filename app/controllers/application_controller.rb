124b8ef81b407a9c432e0a3ce055d18190c2993d
kumbafu <mutero_d@yahoo.com>
2011-10-11 13:14:09 -0500

    version bump

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -30,7 +30,7 @@ class ApplicationController < ActionController::Base
 
   #
   # Application version
-  APP_VERSION = "2.0.0"
+  APP_VERSION = "2.0.1"
 
 
   def redirect_with_message(path, message_type, message)
