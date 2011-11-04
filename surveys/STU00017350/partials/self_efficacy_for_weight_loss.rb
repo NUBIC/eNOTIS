b2419ddfc5125a5782212251a4bfd717ce11ff3e
kumbafu <mutero_d@yahoo.com>
2011-11-04 11:40:57 -0500

    fixed forms

--- surveys/STU00017350/partials/self_efficacy_for_weight_loss.rb
+++ surveys/STU00017350/partials/self_efficacy_for_weight_loss.rb
@@ -11,113 +11,26 @@ def self_efficacy_for_weight_loss
       <br/> 4 = Very confident
       <br/> 5 = Completely confident <br/></b>"
 
-    grid "I am under a lot of stress." do
+    grid_1 "" do
       a "1"
       a "2"
       a "3"
       a "4"
       a "5"
-      q "Not at all confident|Completely confident" , :pick => :one
-    end
+      q_1 "I am under a lot of stress." , :pick => :one
+      q_2 "I am depressed." , :pick => :one
+      q_3 "I am anxious." , :pick => :one
+      q_4 "I feel I don't have the time." , :pick => :one
+      q_5 "I don't feel like it." , :pick => :one
+      q_6 "I am busy." , :pick => :one
+      q_7 "I am in a rush." , :pick => :one
+      q_8 "I am tired." , :pick => :one
+      q_9 "Healthy foods are not available." , :pick => :one
+      q_10 "I am travelling." , :pick => :one
+      q_11 "I only have unhealthy foods at home." , :pick => :one
+      q_12 "I am eating at a restaurant." , :pick => :one
+      q_13 "I am eating fast foods." , :pick => :one
 
-    grid "I am depressed." do
-      a "1"
-      a "2"
-      a "3"
-      a "4"
-      a "5"
-      q "Not at all confident|Completely confident" , :pick => :one
-    end
-    
-    grid "I am anxious." do
-      a "1"
-      a "2"
-      a "3"
-      a "4"
-      a "5"
-      q "Not at all confident|Completely confident" , :pick => :one
-    end        
-    grid "I feel I don't have the time." do
-      a "1"
-      a "2"
-      a "3"
-      a "4"
-      a "5"
-      q "Not at all confident|Completely confident" , :pick => :one
-    end
-    grid "I don't feel like it." do
-      a "1"
-      a "2"
-      a "3"
-      a "4"
-      a "5"
-      q "Not at all confident|Completely confident" , :pick => :one
-    end
-    grid "I am busy." do
-      a "1"
-      a "2"
-      a "3"
-      a "4"
-      a "5"
-      q "Not at all confident|Completely confident" , :pick => :one
-    end
-    grid "I am in a rush." do
-      a "1"
-      a "2"
-      a "3"
-      a "4"
-      a "5"
-      q "Not at all confident|Completely confident" , :pick => :one
-    end
-    grid "I am tired." do
-      a "1"
-      a "2"
-      a "3"
-      a "4"
-      a "5"
-      q "Not at all confident|Completely confident" , :pick => :one
-    end                
-    grid "Healthy foods are not available." do
-      a "1"
-      a "2"
-      a "3"
-      a "4"
-      a "5"
-      q "Not at all confident|Completely confident" , :pick => :one
-    end
-    grid "I am travelling." do
-      a "1"
-      a "2"
-      a "3"
-      a "4"
-      a "5"
-      q "Not at all confident|Completely confident" , :pick => :one
-    end  
-    grid "I only have unhealthy foods at home." do
-      a "1"
-      a "2"
-      a "3"
-      a "4"
-      a "5"
-      q "Not at all confident|Completely confident" , :pick => :one
-    end      
-    
-    grid "I am eating at a restaurant." do
-      a "1"
-      a "2"
-      a "3"
-      a "4"
-      a "5"
-      q "Not at all confident|Completely confident" , :pick => :one
-    end    
-
-    grid "I am eating fast foods." do
-      a "1"
-      a "2"
-      a "3"
-      a "4"
-      a "5"
-      q "Not at all confident|Completely confident" , :pick => :one
     end
 
  
@@ -132,167 +45,32 @@ def self_efficacy_for_weight_loss
       <br/> 4 = Very confident
       <br/> 5 = Completely confident <br/></b>"
     
-    grid "I am under a lot of stress." do
+    grid_2 "I am under a lot of stress." do
       a "1"
       a "2"
       a "3"
       a "4"
       a "5"
-      q "Not at all confident|Completely confident" , :pick => :one
+
+      q_2a "I am under a lot of stress." , :pick => :one
+      q_2b "I am depressed." , :pick => :one
+      q_2c "I am anxious." , :pick => :one
+      q_2d "I feel I don't have the time." , :pick => :one
+      q_2e "I don't feel like it." , :pick => :one
+      q_2f "I am busy." , :pick => :one
+      q_2g "I am alone." , :pick => :one
+      q_2h "I have to exercise alone." , :pick => :one
+      q_2i "My exercise partner decides not to exercise that day" , :pick => :one
+      q_2j "I don't have access to exercise equipment." , :pick => :one
+      q_2k "I am travelling." , :pick => :one
+      q_2l "My gym is closed." , :pick => :one
+      q_2m "My friends don't want me to exercise." , :pick => :one
+      q_2n "I am spending time with friends or family who do not exercise.",:pick => :one
+      q_2o "It's raining or snowing.",:pick => :one
+      q_2p "It's cold outside.",:pick => :one
+      q_2q "The roads or sidewalks are snowy.",:pick => :one
     end
 
-    grid "I am depressed." do
-      a "1"
-      a "2"
-      a "3"
-      a "4"
-      a "5"
-      q "Not at all confident|Completely confident" , :pick => :one
-    end
-
-    grid "I am anxious." do
-      a "1"
-      a "2"
-      a "3"
-      a "4"
-      a "5"
-      q "Not at all confident|Completely confident" , :pick => :one
-    end
-
-    grid "I feel I don't have the time." do
-      a "1"
-      a "2"
-      a "3"
-      a "4"
-      a "5"
-      q "Not at all confident|Completely confident" , :pick => :one
-    end
-
-    grid "I don't feel like it." do
-      a "1"
-      a "2"
-      a "3"
-      a "4"
-      a "5"
-      q "Not at all confident|Completely confident" , :pick => :one
-    end
-
-    grid "I am busy." do
-      a "1"
-      a "2"
-      a "3"
-      a "4"
-      a "5"
-      q "Not at all confident|Completely confident" , :pick => :one
-    end
-
-    grid "I am alone." do
-      a "1"
-      a "2"
-      a "3"
-      a "4"
-      a "5"
-      q "Not at all confident|Completely confident" , :pick => :one
-    end
-
-    grid "I have to exercise alone." do
-      a "1"
-      a "2"
-      a "3"
-      a "4"
-      a "5"
-      q "Not at all confident|Completely confident" , :pick => :one
-    end
-
-    grid "My exercise partner decides not to exercise that day." do
-      a "1"
-      a "2"
-      a "3"
-      a "4"
-      a "5"
-      q "Not at all confident|Completely confident" , :pick => :one
-    end
-
-    grid "I don't have access to exercise equipment." do
-      a "1"
-      a "2"
-      a "3"
-      a "4"
-      a "5"
-      q "Not at all confident|Completely confident" , :pick => :one
-    end
-
-    grid "I am travelling." do
-      a "1"
-      a "2"
-      a "3"
-      a "4"
-      a "5"
-      q "Not at all confident|Completely confident" , :pick => :one
-    end
-
-    grid "My gym is closed." do
-      a "1"
-      a "2"
-      a "3"
-      a "4"
-      a "5"
-      q "Not at all confident|Completely confident" , :pick => :one
-    end
-
-    grid "My friends don't want me to exercise." do
-      a "1"
-      a "2"
-      a "3"
-      a "4"
-      a "5"
-      q "Not at all confident|Completely confident" , :pick => :one
-    end
-
-    grid "My significant other doesn't want me to exercise." do
-      a "1"
-      a "2"
-      a "3"
-      a "4"
-      a "5"
-      q "Not at all confident|Completely confident" , :pick => :one
-    end
-
-    grid "I am spending time with friends or family who do not exercise." do
-      a "1"
-      a "2"
-      a "3"
-      a "4"
-      a "5"
-      q "Not at all confident|Completely confident" , :pick => :one
-    end
-
-    grid "It's raining or snowing." do
-      a "1"
-      a "2"
-      a "3"
-      a "4"
-      a "5"
-      q "Not at all confident|Completely confident" , :pick => :one
-    end
-    
-    grid "It's cold outside." do
-      a "1"
-      a "2"
-      a "3"
-      a "4"
-      a "5"
-      q "Not at all confident|Completely confident" , :pick => :one
-    end
-
-    grid "The roads or sidewalks are snowy." do
-      a "1"
-      a "2"
-      a "3"
-      a "4"
-      a "5"
-      q "Not at all confident|Completely confident" , :pick => :one
-    end
     
     label_3 "This questionnare looks at how confident you are about <b><u>eating fewer calories</u></b> when other things get in the way. 
       Read the following items and write the number that best epresses how each item relates to you. 
@@ -305,123 +83,29 @@ def self_efficacy_for_weight_loss
        <br/> 4 = Very confident
        <br/> 5 = Completely confident <br/></b>"
 
-     grid "I am under a lot of stress." do
+     grid_3 "" do
        a "1"
        a "2"
        a "3"
        a "4"
        a "5"
-       q "Not at all confident|Completely confident" , :pick => :one
+
+       q_3a "I am under a lot of stress." , :pick => :one
+       q_3b "I am depressed." , :pick => :one
+       q_3c "I am anxious." , :pick => :one
+       q_3d "I feel I don't have the time." , :pick => :one
+       q_3e "I don't feel like it." , :pick => :one
+       q_3f "I am busy." , :pick => :one
+       q_3g "I am in a rush." , :pick => :one
+       q_3h "I am tired." , :pick => :one
+       q_3i "Reduced calorie foods are not available." , :pick => :one
+       q_3j "I am traveling." , :pick => :one
+       q_3k "I only have high calorie foods at home." , :pick => :one
+       q_3l "I am eating at a restaurant.",:pick=>:one
+       q_3m "I am eating fast food.",:pick=>:one
      end
 
-     grid "I am depressed." do
-       a "1"
-       a "2"
-       a "3"
-       a "4"
-       a "5"
-       q "Not at all confident|Completely confident" , :pick => :one
-     end
 
-     grid "I am anxious." do
-       a "1"
-       a "2"
-       a "3"
-       a "4"
-       a "5"
-       q "Not at all confident|Completely confident" , :pick => :one
-     end
-
-     grid "I feel I don't have the time." do
-       a "1"
-       a "2"
-       a "3"
-       a "4"
-       a "5"
-       q "Not at all confident|Completely confident" , :pick => :one
-     end
-
-     grid "I don't feel like it." do
-       a "1"
-       a "2"
-       a "3"
-       a "4"
-       a "5"
-       q "Not at all confident|Completely confident" , :pick => :one
-     end
-     
-     grid "I am busy." do
-       a "1"
-       a "2"
-       a "3"
-       a "4"
-       a "5"
-       q "Not at all confident|Completely confident" , :pick => :one
-     end
-
-     grid "I am in a rush." do
-       a "1"
-       a "2"
-       a "3"
-       a "4"
-       a "5"
-       q "Not at all confident|Completely confident" , :pick => :one
-     end
-
-     grid "I am tired." do
-       a "1"
-       a "2"
-       a "3"
-       a "4"
-       a "5"
-       q "Not at all confident|Completely confident" , :pick => :one
-     end
-
-     grid "Reduced calorie foods are not available." do
-       a "1"
-       a "2"
-       a "3"
-       a "4"
-       a "5"
-       q "Not at all confident|Completely confident" , :pick => :one
-     end
-
-     grid "I am traveling." do
-       a "1"
-       a "2"
-       a "3"
-       a "4"
-       a "5"
-       q "Not at all confident|Completely confident" , :pick => :one
-     end
-     
-     grid "I only have high calorie foods at home." do
-       a "1"
-       a "2"
-       a "3"
-       a "4"
-       a "5"
-       q "Not at all confident|Completely confident" , :pick => :one
-     end
-
-     grid "I am eating at a restaurant." do
-       a "1"
-       a "2"
-       a "3"
-       a "4"
-       a "5"
-       q "Not at all confident|Completely confident" , :pick => :one
-     end
-     
-     grid "I am eating fast food." do
-       a "1"
-       a "2"
-       a "3"
-       a "4"
-       a "5"
-       q "Not at all confident|Completely confident" , :pick => :one
-     end
-     
      label_4 "This questionnare looks at how confident you are about <b><u>eating less saturated fat</u></b> when other things get in the way. 
       Read the following items and write the number that best epresses how each item relates to you. 
       Please answer using the following 5-point scale:
