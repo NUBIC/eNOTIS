1bc413561488825d9fe57870d3777c2f130ea3d1
Jeff Lunt <jeff.lunt@northwestern.edu>
2011-11-08 16:45:46 -0600

    Latest versions of the test coverage.

--- spec/models/survey_group_spec.rb
+++ spec/models/survey_group_spec.rb
@@ -10,7 +10,10 @@ describe SurveyGroup do
  end
  
  it "should return the next survey in a group when 'next' is called" do
-   @survey_group.next(@response_set).should be @first_survey
+   next_squential_response_set = @survey_group.next(@response_set)
+   
+   next_sequential_response_set.class.should be ResponseSet.class
+   next_sequential_response_set.should_not be nil
  end
  
  it "should, if the progression is sequential, give the surveys in the correct order, given subsequent calls to 'next' when the current survey is completed"
@@ -18,6 +21,8 @@ describe SurveyGroup do
  it "should prevent you from moving to another survey within the group, if the current survey is not yet complete"
  
  it "random progression should actually be somewhat random"
+ 
+ it "random progression cannot return the current survey from the next method"
  
  it "should ignore forms that are inactive"
  
