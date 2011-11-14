971721ee2c36d557cfcb0f783dc89a5a53e9008b
Jeff Lunt <jeff.lunt@northwestern.edu>
2011-11-14 14:57:27 -0600

    References #6457 - Latest survey changes.

--- surveys/STU00017350/partials/promis_measure_qol.rb
+++ surveys/STU00017350/partials/promis_measure_qol.rb
@@ -3,11 +3,11 @@ def promis_measure_qol
     label "Please respond to each item"
 
     grid do
-      a_1 "1 = Poor"
-      a_2 "2 = Fair"
-      a_3 "3 = Good"
-      a_4 "4 = Very good"
-      a_5 "5 = Excellent"
+      a_1 "1 = Poor", :display_order => 0
+      a_2 "2 = Fair", :display_order => 1
+      a_3 "3 = Good", :display_order => 2
+      a_4 "4 = Very good", :display_order => 3
+      a_5 "5 = Excellent", :display_order => 4
       
       q_1 "In general, would you say your health is:", :pick => :one
       q_2 "In general, would you say your quality of life is:", :pick => :one
@@ -15,35 +15,35 @@ def promis_measure_qol
       q_4 "In general, how would you rate your mental health, including your mood and your ability to think?", :pick => :one
       q_5 "In general, how would you rate your satisfaction with your social activities and relationships?", :pick => :one
       q_6 "In general, please rate how well you carry out your usual social activities and roles. (This includes activities at home, at work and in 
-      your community, and responsibilites as a parent, child, spouse, employee, friend, etc.)", :pick => :one
+      your community, and responsibilities as a parent, child, spouse, employee, friend, etc.)", :pick => :one
     end
        
     grid do
-      a_1 "1 = Not at all"
-      a_2 "2 = A little"
-      a_3 "3 = Moderately"
-      a_4 "4 = Mostly"
-      a_5 "5 = Completely"
+      a_1 "1 = Not at all", :display_order => 0
+      a_2 "2 = A little", :display_order => 1
+      a_3 "3 = Moderately", :display_order => 2
+      a_4 "4 = Mostly", :display_order => 3
+      a_5 "5 = Completely", :display_order => 4
       
       q "To what extent are you able to carry out your everyday physical activities such as walking, climbing stairs, carrying groceries, or moving a chair?", :pick => :one
     end     
     
     grid do
-      a_1 "1 = Always"
-      a_2 "2 = Often"
-      a_3 "3 = Sometimes"
-      a_4 "4 = Rarely"
-      a_5 "5 = Never"
+      a_1 "1 = Always", :display_order => 0
+      a_2 "2 = Often", :display_order => 1
+      a_3 "3 = Sometimes", :display_order => 2
+      a_4 "4 = Rarely", :display_order => 3
+      a_5 "5 = Never", :display_order => 4
                         
       q "In the past 7 days, how often have you been bothered by emotional problems such as feeling anxious, depressed or irritable?", :pick => :one
     end  
     
     grid do
-      a_1 "1 = Very severe"
-      a_2 "2 = Severe"
-      a_3 "3 = Moderate"
-      a_4 "4 = Mild"
-      a_5 "5 = None"
+      a_1 "1 = Very severe", :display_order => 0
+      a_2 "2 = Severe", :display_order => 1
+      a_3 "3 = Moderate", :display_order => 2
+      a_4 "4 = Mild", :display_order => 3
+      a_5 "5 = None", :display_order => 4
             
       q "In the past 7 days, how would you rate your fatigue on average?", :pick => :one
     end
