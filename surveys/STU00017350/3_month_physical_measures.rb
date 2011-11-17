5e90db20c34b60bacb1e2afb65d4e3e940e78768
kumbafu <mutero_d@yahoo.com>
2011-11-17 10:50:03 -0600

    updated weight and waist to float values

--- surveys/STU00017350/3_month_physical_measures.rb
+++ surveys/STU00017350/3_month_physical_measures.rb
@@ -1,11 +1,11 @@
 survey "3 month - Physical Measures" ,:irb_number=>'STU00017350' do
   section "Weight, WC, and Blood Pressure" do
     q_weight "Weight (pounds):"
-    a :string
+    a :float
     
     q_waist "Waist Circumference",
     :help_text => "(cm)"
-    a :string
+    a :float
     
     q_blood_pressure "Blood Pressure",
     :help_text => "systolic/diastolic (mmHg)"
