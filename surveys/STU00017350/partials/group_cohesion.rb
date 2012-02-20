0b6993f4158060be394100b7e80a5a49c47a869d
kumbafu <mutero_d@yahoo.com>
2012-02-20 14:58:38 -0600

    updated engaged 6 month surveys

--- surveys/STU00017350/partials/group_cohesion.rb
+++ surveys/STU00017350/partials/group_cohesion.rb
@@ -1,59 +1,21 @@
 def group_cohesion
   section "GC" do
-    label "Please answer the following questions with regards to how you feel to how you feel about being a part of your ENGAGED team."
+    grid "Please answer the following questions with regards to how you feel about being a part of your ENGAGED team." do 
      
-    q_1 "I feel that I belong to this group", :pick => :one
-    a_1 "Strongly disagree"
-    a_2 "Quite disagree"
-    a_3 "Slightly disagree"
-    a_4 "Neither"
-    a_5 "Slightly agree"
-    a_6 "Quite agree"
-    a_7 "Strongly agree"
+      a_1 "Strongly disagree"
+      a_2 "Quite disagree"
+      a_3 "Slightly disagree"
+      a_4 "Neither"
+      a_5 "Slightly agree"
+      a_6 "Quite agree"
+      a_7 "Strongly agree"
+      q_1 "I feel that I belong to this group", :pick => :one
+      q_2 "I am happy to be part of this group", :pick => :one
+      q_3 "I see myself as part of this group", :pick => :one
+      q_4 "This group is one of the best anywhere", :pick => :one
+      q_5 "I feel that I am a member of this group", :pick => :one
+      q_6 "I am content to be a part of this group", :pick => :one
+    end
 
-    q_2 "I am happy to be part of this group", :pick => :one
-    a_1 "Strongly disagree"
-    a_2 "Quite disagree"
-    a_3 "Slightly disagree"
-    a_4 "Neither"
-    a_5 "Slightly agree"
-    a_6 "Quite agree"
-    a_7 "Strongly agree"
-       
-    q_3 "I see myself as part of this group", :pick => :one
-    a_1 "Strongly disagree"
-    a_2 "Quite disagree"
-    a_3 "Slightly disagree"
-    a_4 "Neither"
-    a_5 "Slightly agree"
-    a_6 "Quite agree"
-    a_7 "Strongly agree"
-    
-    q_4 "This group is one of the best anywhere", :pick => :one
-    a_1 "Strongly disagree"
-    a_2 "Quite disagree"
-    a_3 "Slightly disagree"
-    a_4 "Neither"
-    a_5 "Slightly agree"
-    a_6 "Quite agree"
-    a_7 "Strongly agree"
-    
-    q_5 "I feel that I am a member of this group", :pick => :one
-    a_1 "Strongly disagree"
-    a_2 "Quite disagree"
-    a_3 "Slightly disagree"
-    a_4 "Neither"
-    a_5 "Slightly agree"
-    a_6 "Quite agree"
-    a_7 "Strongly agree"
-    
-    q_6 "I am content to be a part of this group", :pick => :one
-    a_1 "Strongly disagree"
-    a_2 "Quite disagree"
-    a_3 "Slightly disagree"
-    a_4 "Neither"
-    a_5 "Slightly agree"
-    a_6 "Quite agree"
-    a_7 "Strongly agree"            
   end
 end
--- surveys/STU00017350/partials/perceived_autonomy_support_received.rb
+++ surveys/STU00017350/partials/perceived_autonomy_support_received.rb
@@ -1,39 +1,17 @@
 def perceived_autonomy_support_received
   section "PAS-G" do
-    label "Instructions. These questions are related to your team members. Teams have different styles, and we would like to know 
-    more about how you have felt about your encounters with your team. Your responces are confidential. Please be honest and candid"
-     
-    grid "If I feel as though a teammate of mine is not pulling his or her weight, I have sometimes used peer pressure to get them on track." do
-      a "-3"
+    grid "Instructions. These questions are related to your team members. Teams have different styles, and we would like to know more about how you have felt about your encounters with your team. Your responses are confidential. Please be honest and candid" do
+      a "-3 Strongly Disagree"
       a "-2"
       a "-1"
       a "0"
       a "1"
       a "2"
-      a "3"
-      q "Strongly Disagree|Strongly Agree" , :pick => :one
+      a "3 Strongly Agree"
+      q "If I feel as though a teammate of mine is not pulling his or her weight, I have sometimes used peer pressure to get them on track." , :pick => :one
+      q "I'm pretty accepting of my teammates level of participation/success in this study, regardless of how well or poorly they're doing" , :pick => :one
+      q "I try to convey to my teammates that my support of them is not contingent on (or tied to) how well they are doing at losing weight." , :pick => :one
     end
 
-    grid "I'm pretty accepting of my teammates level of participation/success in this study, regardless of how well or poorly they're doing" do
-      a "-3"
-      a "-2"
-      a "-1"
-      a "0"
-      a "1"
-      a "2"
-      a "3"
-      q "Strongly Disagree|Strongly Agree" , :pick => :one
-    end
-           
-    grid "I try to convey to my teammates that my support of the is not contingent on (or tied to) how well they are doing at losing weight." do
-      a "-3"
-      a "-2"
-      a "-1"
-      a "0"
-      a "1"
-      a "2"
-      a "3"
-      q "Strongly Disagree|Strongly Agree" , :pick => :one
-    end  
   end
 end
